e45902eb01cb0445c854e11bcc60495cf5d44b4c
Timothy <timothy0336@hotmail.com>
2021-01-31 10:13:31 +0800

    fix: exclude draft posts when creating tag paths

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -38,7 +38,7 @@ export async function getStaticProps({ params }) {
   return { props: { posts: filteredPosts, tag: params.tag } }
 }
 
-export default function Blog({ posts, tag }) {
+export default function Tag({ posts, tag }) {
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   return (
