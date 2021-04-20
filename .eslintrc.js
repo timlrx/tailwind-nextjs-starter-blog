2ebdcb81f5947c227d3220cd52be7bd2f41e3ea2
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2021-04-20 17:04:11 +0200

    add installed but missing eslint-plugin-react-hooks to .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended',
   ],
