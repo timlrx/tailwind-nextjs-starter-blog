d18c9bf67fa69acc48332956b8b1a59711bb3cab
Ngineer101 <nicolaasb@pm.me>
2021-05-10 08:05:52 +0200

    Added react project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,3 +1,10 @@
-const projectsData = []
+const projectsData = [
+  {
+    title: 'React image & video lightbox',
+    description: `A ReactJS lightbox that supports images & videos. Optimized for mobile UI, but can be used on desktop as well.`,
+    imgSrc: '/static/images/react.png',
+    href: 'https://github.com/Ngineer101/react-image-video-lightbox',
+  },
+]
 
 export default projectsData
