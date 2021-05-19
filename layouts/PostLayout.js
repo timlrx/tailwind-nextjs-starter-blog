2ebf74dcc50ff9d87e99284279a05b56597fb72b
Dr. Derek Austin <derekraustin@gmail.com>
2021-05-19 14:39:36 -0500

    fix: make siteMetadata.twitter optional to fix TypeError

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -51,15 +51,19 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
                     <dl className="text-sm font-medium leading-5 whitespace-nowrap">
                       <dt className="sr-only">Name</dt>
                       <dd className="text-gray-900 dark:text-gray-100">{siteMetadata.author}</dd>
-                      <dt className="sr-only">Twitter</dt>
-                      <dd>
-                        <Link
-                          href={siteMetadata.twitter}
-                          className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                        >
-                          {siteMetadata.twitter.replace('https://twitter.com/', '@')}
-                        </Link>
-                      </dd>
+                      {typeof siteMetadata.twitter === 'string' && (
+                        <>
+                          <dt className="sr-only">Twitter</dt>
+                          <dd>
+                            <Link
+                              href={siteMetadata.twitter}
+                              className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+                            >
+                              {siteMetadata.twitter.replace('https://twitter.com/', '@')}
+                            </Link>
+                          </dd>
+                        </>
+                      )}
                     </dl>
                   </li>
                 </ul>
