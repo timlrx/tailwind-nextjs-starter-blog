9978910cca8c34e7da3b9361dca427d608dcc2c0
Timothy Lin <timothy.lin@cylynx.io>
2021-05-20 23:57:13 +0800

    feat: copy code button

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import CustomLink from './Link'
+import Pre from './Pre'
 
 const MDXComponents = {
   Image,
   a: CustomLink,
+  pre: Pre,
 }
 
 export default MDXComponents
