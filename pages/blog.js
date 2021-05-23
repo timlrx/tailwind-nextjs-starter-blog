dc4cdad8e3a205bc91336080efd1f0105cefbf7e
Ahmad Al Maaz <Music47ell@gmail.com>
2021-05-23 15:40:03 +0300

    Change posts per pade value to 10

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,7 @@ import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSeo } from '@/components/SEO'
 
-export const POSTS_PER_PAGE = 3
+export const POSTS_PER_PAGE = 10
 
 export async function getStaticProps() {
   const getPosts = await getAllFilesFrontMatter('blog')
