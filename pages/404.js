316e869fa379b136490e165f24c779115ebe3767
Ngineer101 <nicolaasb@pm.me>
2021-05-24 12:42:07 +0200

    Added robots.txt and modified 404 page

--- pages/404.js
+++ pages/404.js
@@ -10,9 +10,11 @@ export default function FourZeroFour() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          Sorry, this page couldn't be found.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">
+          But don't worry, you can find plenty of other things on the homepage.
+        </p>
         <Link href="/">
           <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500">
             Back to homepage
