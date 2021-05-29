eb1f95e3d9961519a63ae984d3eafca508633152
Timothy Lin <timothy.lin@cylynx.io>
2021-05-29 18:10:54 +0800

    style: adjust copy button style

--- components/Pre.js
+++ components/Pre.js
@@ -26,7 +26,7 @@ const Pre = (props) => {
         <button
           aria-label="Copy code"
           type="button"
-          className={`absolute right-3 w-8 h-8 p-1 rounded border-2 bg-gray-700 dark:bg-gray-800 ${
+          className={`absolute right-2 top-2 w-8 h-8 p-1 rounded border-2 bg-gray-700 dark:bg-gray-800 ${
             copied
               ? 'focus:outline-none focus:border-green-400 border-green-400'
               : 'border-gray-300'
