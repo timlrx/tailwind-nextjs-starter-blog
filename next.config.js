5945a724913f7313c9b58ce6dfaa917901828b25
Timothy Lin <timothy0336@hotmail.com>
2021-06-09 23:37:58 +0800

    fix: patch vercel bug which directs user to index.xml

--- next.config.js
+++ next.config.js
@@ -38,4 +38,17 @@ module.exports = withBundleAnalyzer({
 
     return config
   },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Rewrite to prevent a problem when deploying at vercel
+        // which directs a user to the index.xml instead of index.html
+        // https://github.com/timlrx/tailwind-nextjs-starter-blog/issues/16
+        {
+          source: '/',
+          destination: '/index',
+        },
+      ],
+    }
+  },
 })
