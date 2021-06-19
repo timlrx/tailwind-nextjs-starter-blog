8688460e6be387d3f3e53090ea3cdc5bff3560e7
aryaveersr <AryaveerSR@users.noreply.github.com>
2021-06-19 19:03:44 +0530

    Compose.js Updates

--- scripts/compose.js
+++ scripts/compose.js
@@ -1,31 +1,56 @@
+'use strict'
 const fs = require('fs')
+var inquirer = require('inquirer')
+inquirer
+  .prompt([
+    {
+      name: 'title',
+      message: 'What shall be the Post Title ?',
+      type: 'input',
+    },
+    {
+      name: 'extention',
+      message: 'What shall be the Post Extention ?',
+      type: 'list',
+      choices: ['md', 'mdx'],
+    },
+    {
+      name: 'draft',
+      message: 'Shall it be a draft ? ( yes/no )',
+      type: 'input',
+    },
+    {
+      name: 'tags',
+      message: 'Any Tags ? Use , to seperate. Leave if no tags',
+      type: 'input',
+    },
+  ])
+  .then((answers) => {
+    // Remove special characters and replace space with -
+    const fileName = answers.title
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9 ]/g, '')
+      .replace(/ /g, '-')
+      .replace(/-+/g, '-');
+    let d = new Date();
+    const date = [
+      d.getFullYear(),
+      ('0' + (d.getMonth() + 1)).slice(-2),
+      ('0' + d.getDate()).slice(-2),
+    ].join('-');
+    var tagArray = answers.tags.split(',');
+    tagArray.forEach((tag,index) => tagArray[index] = tag.trim());
+    const tags = "'" + tagArray.join("','") + "'"
 
-const args = process.argv.slice(2)
-const title = args[0]
-const ext = typeof args[1] !== 'undefined' ? args[1] : 'mdx'
-// Remove special characters and replace space with -
-const fileName = title
-  .toLowerCase()
-  .replace(/[^a-zA-Z0-9 ]/g, '')
-  .replace(/ /g, '-')
-  .replace(/-+/g, '-')
-let d = new Date()
-const date = [
-  d.getFullYear(),
-  ('0' + (d.getMonth() + 1)).slice(-2),
-  ('0' + d.getDate()).slice(-2),
-].join('-')
-
-const frontMatter = `---
-title: ${title}
-date: '${date}'
-tags: []
-draft: true
-summary: 
-images: []
----
-`
-
-fs.writeFile(`data/blog/${fileName}.${ext}`, frontMatter, (err) => {
-  if (err) throw err
-})
+    const frontMatter = `---\ntitle: ${answers.title?answers.title:'Untitled'}\ndate: '${date}'\ntags: [${answers.tags?tags:''}]\ndraft: ${answers.draft.toLowerCase() == 'no' ? 'false' : 'true'}\nsummary: \nimages: []\n---`
+    fs.writeFile(`data/blog/${fileName?fileName:'untitled'}.${answers.extention?answers.extention:'md'}`, frontMatter, (err) => {
+      if (err) throw err
+    })
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment")
+    } else {
+      console.log('Something Went Wrong, Oopsie!')
+    }
+  })
