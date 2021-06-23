8efbb173af3a081f1d3457d50190c9411d0fd2fe
Timothy Lin <timothy.lin@cylynx.io>
2021-06-23 18:32:29 +0800

    chore: update to next 11 and tailwind 2.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        blue: colors.lightBlue,
+        blue: colors.sky,
         code: {
           green: '#b5f4a5',
           yellow: '#ffe484',
