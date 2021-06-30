5ec8eb3a993d5d5011757e0be7ecc0ec5f9e128a
Ngineer101 <nicolaasb@pm.me>
2021-06-30 12:00:43 +0200

    Added open analytics page

--- components/Footer.js
+++ components/Footer.js
@@ -21,6 +21,8 @@ export default function Footer() {
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
+          <div>{` • `}</div>
+          <Link href="/analytics">Analytics</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
