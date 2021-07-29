855b80a7efb927e9a10dec8b50115dfd55760682
sage <laymonage@gmail.com>
2021-07-29 07:55:42 +0700

    fix: rename giscus-frame class to giscus

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -42,7 +42,7 @@ const Giscus = ({ mapping }) => {
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
       {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>}
-      <div className="giscus-frame" id={COMMENTS_ID} />
+      <div className="giscus" id={COMMENTS_ID} />
     </div>
   )
 }
