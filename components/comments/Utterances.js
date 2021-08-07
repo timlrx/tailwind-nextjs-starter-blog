466e873bbeb1f01adb6a06d2536a590b3b76cbbe
Gautier Arcin <gauta82@gmail.com>
2021-08-07 20:55:18 +0200

    chore: minor translation & date

--- components/comments/Utterances.js
+++ components/comments/Utterances.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { useTheme } from 'next-themes'
 
 import siteMetadata from '@/data/siteMetadata'
+import useTranslation from 'next-translate/useTranslation'
 
 const Utterances = ({ issueTerm }) => {
+  const { t } = useTranslation()
   const [enableLoadComments, setEnabledLoadComments] = useState(true)
   const { theme, resolvedTheme } = useTheme()
   const commentsTheme =
@@ -36,7 +38,7 @@ const Utterances = ({ issueTerm }) => {
   // Added `relative` to fix a weird bug with `utterances-frame` position
   return (
     <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300">
-      {enableLoadComments && <button onClick={LoadComments}>Load Comments</button>}
+      {enableLoadComments && <button onClick={LoadComments}>{t('common:comment')}</button>}
       <div className="utterances-frame relative" id={COMMENTS_ID} />
     </div>
   )
