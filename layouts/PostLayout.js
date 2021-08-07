ebd24e3a9d5c28e7c2bb5aa241e42408bdf96919
Gautier Arcin <gauta82@gmail.com>
2021-08-08 00:17:24 +0200

    chore: fixed page i18n

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -116,7 +116,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                     {prev && (
                       <div>
                         <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                          {t('common:prev')}
+                          {t('common:preva')}
                         </h2>
                         <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                           <Link href={`/blog/${prev.slug}`}>{prev.title}</Link>
@@ -126,7 +126,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                     {next && (
                       <div>
                         <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                          {t('common:next')}
+                          {t('common:nexta')}
                         </h2>
                         <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
                           <Link href={`/blog/${next.slug}`}>{next.title}</Link>
