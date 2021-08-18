7b664cba75ea7243ddbe235bf62030e4900fa661
Gautier Arcin <gauta82@gmail.com>
2021-08-18 17:33:15 +0200

    feat: chimpunk

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
+import { FormSuscribe } from '@/components/FormSuscribe'
+
 const MAX_DISPLAY = 5
 
 export async function getStaticProps() {
@@ -17,6 +19,7 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      {siteMetadata.newsletter.provider !== '' && <FormSuscribe />}
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
