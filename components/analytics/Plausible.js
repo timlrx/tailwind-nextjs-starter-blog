52ade1c9f3ab1fe414754f8ac206eafed64eda83
Timothy Lin <timothy.lin@cylynx.io>
2021-09-05 18:39:53 +0800

    chore: upgrade linter to 11.1.2 and fix lint issues

--- components/analytics/Plausible.js
+++ components/analytics/Plausible.js
@@ -10,7 +10,7 @@ const PlausibleScript = () => {
         data-domain={siteMetadata.analytics.plausibleDataDomain}
         src="https://plausible.io/js/plausible.js"
       />
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="plausible-script">
         {`
             window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
         `}
