bb7961068806cb6713bfc700275fa735a14c70d9
Timothy Lin <timothy.lin@cylynx.io>
2021-09-11 16:41:22 +0800

    chore: remove console log

--- pages/api/convertkit.js
+++ pages/api/convertkit.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default async (req, res) => {
   const { email } = req.body
-  console.log('email : ', email)
 
   if (!email) {
     return res.status(400).json({ error: 'Email is required' })
