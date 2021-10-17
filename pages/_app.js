cc2ecb1c3be7ada8047fbb30c6bcdacc4601f602
Timothy Lin <timothy.lin@cylynx.io>
2021-10-17 16:13:09 +0800

    feat: add custom next-remote-watch

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,9 @@ import Head from 'next/head'
 
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
+import { ClientReload } from '@/components/ClientReload'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,6 +16,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
+      {isDevelopment && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
