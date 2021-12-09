31d16aa6b4d434c00a4820f5d80051e13a4d311c
Ngineer101 <nicolaasb@pm.me>
2021-12-09 11:54:30 +0100

    Center text on author layout

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -23,9 +23,11 @@ export default function AuthorLayout({ children, frontMatter }) {
               height="192px"
               className="w-48 h-48 rounded-full"
             />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight text-center">
+              {name}
+            </h3>
+            <div className="text-gray-500 dark:text-gray-400 text-center">{occupation}</div>
+            <div className="text-gray-500 dark:text-gray-400 text-center">{company}</div>
             <div className="flex pt-6 space-x-3">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
