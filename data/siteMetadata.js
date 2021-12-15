1362d817df87d77a7a575a0c2997aa738d0b5a34
Timothy <timothy0336@hotmail.com>
2021-12-15 16:45:11 +0800

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
-  theme: 'dark', // dark or light
+  theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
