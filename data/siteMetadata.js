4bd69bc47aaf6ef1f2ad7052524293217bd64854
llovvoll <Rick.Jiang@aol.com>
2021-12-15 15:46:20 +0800

    feat: customize default theme

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,6 +4,7 @@ const siteMetadata = {
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
+  theme: 'dark', // dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
