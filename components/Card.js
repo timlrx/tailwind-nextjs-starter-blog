9d928b1c8841b797d2b2e2816d5c634a1d3f64db
Irvin <irvinln@gmail.com>
2021-12-27 11:42:09 +0800

    make project images optional with height adjustment

--- components/Card.js
+++ components/Card.js
@@ -3,9 +3,23 @@ import Link from './Link'
 
 const Card = ({ title, description, imgSrc, href }) => (
   <div className="p-4 md:w-1/2 md" style={{ maxWidth: '544px' }}>
-    <div className="h-full overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
-      {href ? (
-        <Link href={href} aria-label={`Link to ${title}`}>
+    <div
+      className={`${
+        imgSrc && 'h-full'
+      } overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700`}
+    >
+      {imgSrc ? (
+        href ? (
+          <Link href={href} aria-label={`Link to ${title}`}>
+            <Image
+              alt={title}
+              src={imgSrc}
+              className="object-cover object-center lg:h-48 md:h-36"
+              width={544}
+              height={306}
+            />
+          </Link>
+        ) : (
           <Image
             alt={title}
             src={imgSrc}
@@ -13,15 +27,9 @@ const Card = ({ title, description, imgSrc, href }) => (
             width={544}
             height={306}
           />
-        </Link>
+        )
       ) : (
-        <Image
-          alt={title}
-          src={imgSrc}
-          className="object-cover object-center lg:h-48 md:h-36"
-          width={544}
-          height={306}
-        />
+        <></>
       )}
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
--- data/projectsData.js
+++ data/projectsData.js
@@ -12,7 +12,7 @@ const projectsData = [
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    // imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
 ]
