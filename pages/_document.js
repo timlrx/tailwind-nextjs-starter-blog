76d33b4c0435a54e44ac0b9c30e3d131972ead30
Karuppusamy <d.karuppusamy@outlook.com>
2022-01-11 00:46:27 +0530

    Update favicons and site.webmanifest

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicons/apple-touch-icon.png"
+          />
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+          <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
           <link
             rel="icon"
             type="image/png"
@@ -19,8 +25,8 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
