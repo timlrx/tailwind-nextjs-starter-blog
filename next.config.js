ae26e093cd887d2bb910d4e56aea387e33c82c01
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2022-01-16 15:32:37 +0100

    Add  *.googletagmanager.com to script-src
    After adding `googleAnalyticsId` analytics did not work because the google script was blocked.

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.googletagmanager.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com cdn.jsdelivr.net;
   img-src * blob: data:;
   media-src 'none';
