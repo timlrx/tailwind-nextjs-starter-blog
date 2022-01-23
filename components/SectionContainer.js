d26be80f19d9896c6f3b4a18158fc849bf14f475
Timothy Lin <timothy.lin@cylynx.io>
2022-01-23 18:03:15 +0800

    try fixing max height

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,10 @@
 export default function SectionContainer({ children }) {
-  return <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return (
+    <div
+      className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0"
+      style={{ maxHeight: '5000em' }}
+    >
+      {children}
+    </div>
+  )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/css/tailwind.css'
 import '@/css/prism.css'
 
 import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
@@ -14,6 +15,9 @@ const isSocket = process.env.SOCKET
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
