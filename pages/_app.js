04a686fafee1b15665fc383f2d5147532ba344ca
Timothy Lin <timothy.lin@cylynx.io>
2022-01-23 17:38:29 +0800

    fix: remove meta tag from _app

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '@/css/tailwind.css'
 import '@/css/prism.css'
 
 import { ThemeProvider } from 'next-themes'
-import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
@@ -15,9 +14,6 @@ const isSocket = process.env.SOCKET
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
