3d8a153839d1ec6b58854804d48bde526c9775c7
Edwin Coronado <edcoronado.17@gmail.com>
2022-01-23 23:36:34 -0800

    Update compose.js

--- scripts/compose.js
+++ scripts/compose.js
@@ -101,7 +101,7 @@ inquirer
       .replace(/ /g, '-')
       .replace(/-+/g, '-')
     const frontMatter = genFrontMatter(answers)
-    if (!fs.existsSync('data/blog', { recursive: true })) fs.mkdirSync('data/blog')
+    if (!fs.existsSync('data/blog')) fs.mkdirSync('data/blog', { recursive: true })
     const filePath = `data/blog/${fileName ? fileName : 'untitled'}.${
       answers.extension ? answers.extension : 'md'
     }`
