2eaac5ae315d98fb43f779fe4daedaeef6eef220
Timothy Lin <timothy.lin@cylynx.io>
2022-01-31 23:58:10 +0800

    prettier tailwind

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,3 @@
 export default function SectionContainer({ children }) {
-  return <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
 }
