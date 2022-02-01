8cadf5a6a68a4ed18c3d3ff67e2a6f923c15f3a3
Alexander Zeilmann <alexander.zeilmann@googlemail.com>
2022-02-01 09:20:15 +0100

    Replace Google Fonts with Fontsource

--- pages/_document.js
+++ pages/_document.js
@@ -22,11 +22,6 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
