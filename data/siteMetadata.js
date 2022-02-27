5f78b298db4228938c7d5cfc65ca717d000b24df
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2022-02-26 20:15:18 -0800

    comment out comments/discussions

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'Software + Cloud Architecture + Business',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://knowsuchagency.vercel.app',
+  siteUrl: 'https://knowsuchagency.com',
   siteRepo: 'https://github.com/knowsuchagency/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
@@ -34,7 +34,7 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: '', // supported providers: giscus, utterances, disqus
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -88,10 +88,10 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(slug)} rel="nofollow">
-                  {'Discuss on Twitter'}
-                </Link>
-                {` • `}
+                {/*<Link href={discussUrl(slug)} rel="nofollow">*/}
+                {/*  {'Discuss on Twitter'}*/}
+                {/*</Link>*/}
+                {/*{` • `}*/}
                 <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
               </div>
               <Comments frontMatter={frontMatter} />
