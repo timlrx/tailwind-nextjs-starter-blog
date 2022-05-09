7318985057fe21b1061789de726891165f2d96fd
Matt Galligan <matt.galligan@gmail.com>
2022-05-09 07:29:07 -0500

    Add my customizations (#1)
    * Add my customizations
    - Changed header links
    - Updated logo.svg
    - Altered projectsData
    - Updated siteMetadata
    - Updated author data
    * Delete a couple default blog posts

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
