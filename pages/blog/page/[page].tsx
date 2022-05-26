9faf523d54d4eefc0e945e446ff5119b0ec11071
Reuben Rapose <54074556+reubence@users.noreply.github.com>
2022-05-26 14:54:50 +0530

    Forgot to add imports

--- pages/blog/page/[page].tsx
+++ pages/blog/page/[page].tsx
@@ -5,6 +5,7 @@ import { allCoreContent } from '@/lib/utils/contentlayer'
 import { POSTS_PER_PAGE } from '../../blog'
 import { InferGetStaticPropsType } from 'next'
 import { allBlogs } from 'contentlayer/generated'
+import { sortedBlogPost } from '../../../lib/utils/contentlayer'
 
 export const getStaticPaths = async () => {
   const totalPosts = allBlogs
