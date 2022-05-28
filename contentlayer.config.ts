8c5c5734319424b8de2fa6df2583cc46ccca8560
Timothy Lin <timothy0336@hotmail.com>
2022-05-28 14:22:41 +0800

    update dependencies

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -68,7 +68,6 @@ export const Authors = defineDocumentType(() => ({
 
 export default makeSource({
   contentDirPath: 'data',
-  contentDirInclude: ['blog', 'authors'],
   documentTypes: [Blog, Authors],
   mdx: {
     cwd: process.cwd(),
