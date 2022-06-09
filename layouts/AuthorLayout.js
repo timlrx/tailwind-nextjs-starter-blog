f4b6035a3b06bfb16b64e594da33aac54d600723
Cory Dransfeldt <hi@coryd.dev>
2022-06-09 10:26:13 -0700

    Fixes inconsistency in styles for divider on author and list layouts in dark mode

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -8,7 +8,7 @@ export default function AuthorLayout({ children, frontMatter }) {
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
-      <div className="divide-y">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
