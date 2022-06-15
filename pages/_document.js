708e4d9a65f0e862fcfe6b385a78c74b68755373
Timothy Lin <timothy0336@hotmail.com>
2022-06-15 22:27:05 +0800

    feat: use prefers-color-scheme for meta theme-color

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,8 @@ class MyDocument extends Document {
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+          <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
