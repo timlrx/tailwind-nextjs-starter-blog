1d7269f3f9ff968231909fb099f220cdbb0dff42
sky cherfa <sherifcherfa@gmail.com>
2022-07-10 12:31:58 -0400

    I make  a project card

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,17 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Ricocchet Exchange',
+    description: `Tired of trying to time the market? Ricocchet Exchange is a platform for investors, hodlers, and builders to invest in their favorite cryptocurrencies.
+    Through the power of Dollar-cost-averaging and Superfluid money streaming, you can set a monthly investment and you can buy crypto by the SECOND.`,
+    imgSrc: '/static/images/ricochetexchange.png',
+    href: 'https://ricochet.exchange',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'Cryptocurrency Price Scraper',
+    description: `Don't spend your time doing things you don't need to. This is a simple web app that scrapes cryptocurrency prices from almost any website.
+    By using Puppeteer to scrape the data, it's easy to get the information you need. for your business and personal use.`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://github.com/Seroxdesign/crypto-price-scraper',
   },
 ]
 
--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Showcasing all the projects I've worked on.
           </p>
         </div>
         <div className="container py-12">
