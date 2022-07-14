e9ea4d33e3b5cbdffaa3bff67b24843911a763ab
sky cherfa <sherifcherfa@gmail.com>
2022-07-14 15:44:50 -0400

    added new project

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,17 +1,24 @@
 const projectsData = [
   {
-    title: 'Ricocchet Exchange',
+    title: 'Ricocchet Exchange - (Production)',
     description: `Tired of trying to time the market? Ricocchet Exchange is a platform for investors, hodlers, and builders to invest in their favorite cryptocurrencies.
     Through the power of Dollar-cost-averaging and Superfluid money streaming, you can set a monthly investment and you can buy crypto by the SECOND.`,
     imgSrc: '/static/images/ricochetexchange.png',
     href: 'https://ricochet.exchange',
   },
   {
-    title: 'Cryptocurrency Price Scraper',
-    description: `Don't spend your time doing things you don't need to. This is a simple web app that scrapes cryptocurrency prices from almost any website.
+    title: 'Crypto Price Scraper - (Production)',
+    description: `Don't spend your time doing things you don't need to. This is a simple web app that automates the scraping and collection of cryptocurrency prices from almost any website.
     By using Puppeteer to scrape the data, it's easy to get the information you need. for your business and personal use.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/puppeteer.jpg',
     href: 'https://github.com/Seroxdesign/crypto-price-scraper',
+  },
+  {
+    title: 'Metrodao - (Development)',
+    description: `If you could decide how public money is spent in your community, would you take the opportunity? - A public-private relations app focused on allowing ecveryone to vote
+    from the comfort of their own home.`,
+    imgSrc: '/static/images/metrodao.jpg',
+    href: 'https://metrodao.us/our-team',
   },
 ]
 
