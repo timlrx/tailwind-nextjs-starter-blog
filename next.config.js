72b51ca9c895bcdbfecf99567ec73f1f78157665
Benjamin Hedrich <code@pagenotfound.de>
2022-08-06 17:57:44 +0200

    Upgrade several node dependencies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+const withPreact = require('next-plugin-preact');
+
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
@@ -52,7 +54,7 @@ const securityHeaders = [
   },
 ]
 
-module.exports = withBundleAnalyzer({
+module.exports = withPreact(withBundleAnalyzer({
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
@@ -72,16 +74,6 @@ module.exports = withBundleAnalyzer({
       use: ['@svgr/webpack'],
     })
 
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
     return config
   },
-})
+}))
--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const globby = require('globby')
-const matter = require('gray-matter')
-const prettier = require('prettier')
-const siteMetadata = require('../data/siteMetadata')
+import * as fs from 'fs';
+import { globby } from 'globby';
+import matter  from 'gray-matter';
+import prettier   from 'prettier';
+import siteMetadata from '../data/siteMetadata.js';
 
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
