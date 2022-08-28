a123e32e0965c0e557f20576df78136e90cb916c
sky cherfa <sherifcherfa@gmail.com>
2022-08-28 09:47:25 -0400

    fixed

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -15,9 +15,6 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
                     {siteMetadata.headerTitle}
