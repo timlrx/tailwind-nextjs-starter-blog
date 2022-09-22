9ae241ec0105d1b83e1aee72e7526c1083bb0755
Arthur Coudouy <arthur.coudouy@gmail.com>
2022-09-22 15:41:57 +0200

    top 6 tools for dev producitivty in 2022

--- imageScript.js
+++ imageScript.js
@@ -1,6 +1,10 @@
 const images = [
-  "public/static/images/do/createappDO.jpg",
-  "public/static/images/general/productivity.png",
+  "public/static/images/productivity/axolo.png",
+  "public/static/images/productivity/dewo.jpg",
+  "public/static/images/productivity/flux.jpg",
+  "public/static/images/productivity/raycast.png",
+  "public/static/images/productivity/timely.png",
+  "public/static/images/productivity/tuple.png",
 ]
 
 var sizeOf = require("image-size")
@@ -9,8 +13,9 @@ function printImageComponentWithSize(imgSrc) {
   sizeOf(imgSrc, function (err, dimensions) {
     const { width, height } = dimensions
     const imgAlt = imgSrc.split("/")[imgSrc.split("/").length - 1].split(".")[0]
+    const src = imgSrc.replace("public", "blog")
     console.log(
-      `<ImageContainer alt={${imgAlt}} src={imgSrc} className="" classNameImage='' width={${width}} height={${height}} /> `
+      `<ImageContainer alt="${imgAlt}" src="/${src}" classNameDiv="" classNameImage='' width={${width}} height={${height}} /> `
     )
   })
 }
