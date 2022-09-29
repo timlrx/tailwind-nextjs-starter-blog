94b53f144f04bb4415316362018bc01a75d4612a
Sydney Cohen <chnsydney@gmail.com>
2022-09-29 14:30:54 +0200

    new bitbucket article

--- imageScript.js
+++ imageScript.js
@@ -1,28 +1,26 @@
-const images = [
-  "public/static/images/productivity/axolo.png",
-  "public/static/images/productivity/dewo.jpg",
-  "public/static/images/productivity/flux.jpg",
-  "public/static/images/productivity/raycast.png",
-  "public/static/images/productivity/timely.png",
-  "public/static/images/productivity/tuple.png",
-]
+// Here put the folder name and it will create a component for each image that he can
+const folder = "public/static/images/bitbucket/"
 
 var sizeOf = require("image-size")
+var fs = require("fs")
+var files = fs.readdirSync("public/static/images/bitbucket/")
 
 function printImageComponentWithSize(imgSrc) {
   sizeOf(imgSrc, function (err, dimensions) {
-    const { width, height } = dimensions
-    const imgAlt = imgSrc.split("/")[imgSrc.split("/").length - 1].split(".")[0]
-    const src = imgSrc.replace("public", "blog")
-    console.log(
-      `<ImageContainer alt="${imgAlt}" src="/${src}" classNameDiv="" classNameImage='' width={${width}} height={${height}} /> `
-    )
+    if (dimensions) {
+      const { width, height } = dimensions
+      const imgAlt = imgSrc.split("/")[imgSrc.split("/").length - 1].split(".")[0]
+      const src = imgSrc.replace("public", "blog")
+      console.log(
+        `<ImageContainer alt="${imgAlt}" src="/${src}" classNameDiv="" classNameImage='' width={${width}} height={${height}} /> `
+      )
+    }
   })
 }
 
 function main() {
-  for (const image of images) {
-    printImageComponentWithSize(image)
+  for (const image of files) {
+    printImageComponentWithSize(`${folder}${image}`)
   }
 }
 
