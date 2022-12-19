60d45c70de6178c7b4d4b27ff2db86e33257729f
sky cherfa <sherifcherfa@gmail.com>
2022-12-19 16:43:37 -0500

    expand portfolio

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,48 +1,45 @@
 const projectsData = [
   {
-    title: 'Ricocchet Exchange - (Production)',
-    description: `Tired of trying to time the market? Ricocchet Exchange is a platform for investors, hodlers, and builders to invest in their favorite cryptocurrencies.
-    Through the power of Dollar-cost-averaging and Superfluid money streaming, you can set a monthly investment and you can buy crypto by the SECOND.`,
+    title: 'Decentra Talks Podcast',
+    description: `Join us for conversations about the future of our financial systems.`,
+    imgSrc: '/static/images/decentra-talks.png',
+    href: 'https://open.spotify.com/episode/4u5faDs9DEPvZMDTwIRR4L?si=45844b55592f4f3a',
+  },
+  {
+    title: 'Ricochet Exchange - (Production)',
+    description: `Dollar Cost Averaging DEX leveraging Superfluid money streams.`,
     imgSrc: '/static/images/ricochetexchange.png',
     href: 'https://ricochet.exchange',
   },
   {
-    title: 'Superfluid Finance - Contract',
+    title: 'Superfluid Finance - (Production)',
     description: `Implementing Etheruem Name Services into the Superfluid Console's search.`,
     imgSrc: '/static/images/superfluid.png',
     href: 'https://console.superfluid.finance',
   },
   {
-    title: 'Tellor Finance - Contract',
-    description: `Redesigning website frontend to improve user experience and interface.`,
+    title: 'Tellor Finance - (Production)',
+    description: `Decentralised onchain tipping and reporting system for token data feeds.`,
     imgSrc: '/static/images/tellor.png',
     href: 'https://fundafeed.herokuapp.com/',
   },
   {
-    title: 'Alluo Finance - Contract',
-    description: `Extending transaction options and interface for Alluo users.`,
+    title: 'DebtDAO - (Production)',
+    description: `Revenue based collateralized borrowing and lending on Ethereum Mainnet.`,
+    imgSrc: '/static/images/debt-dao.png',
+    href: 'https://debtdao.finance/#/market',
+  },
+  {
+    title: 'Alluo Finance - (Production)',
+    description: `DCA and earn yield while you Hodl your crypto, in our trustless and decentralised dApp.`,
     imgSrc: '/static/images/alluo.jpg',
     href: 'https://app.alluo.finance/',
   },
   {
     title: 'Crypto Price Scraper - (Production)',
-    description: `Don't spend your time doing things you don't need to. This is a simple web app that automates the scraping and collection of cryptocurrency prices from almost any website.
-    By using Puppeteer to scrape the data, it's easy to get the information you need. for your business and personal use.`,
+    description: `Scrape the prices of Cryptocurrencies from any platform.`,
     imgSrc: '/static/images/puppeteer.jpg',
     href: 'https://github.com/Seroxdesign/crypto-price-scraper',
-  },
-  {
-    title: 'Metrodao - (Development)',
-    description: `If you could decide how public money is spent in your community, would you take the opportunity? - A public-private relations app focused on allowing ecveryone to vote
-    from the comfort of their own home.`,
-    imgSrc: '/static/images/metrodao.jpg',
-    href: 'https://metrodao.us/our-team',
-  },
-  {
-    title: 'Defi Talks Podcast',
-    description: `Join us for conversations about the future of our financial systems.`,
-    imgSrc: '/static/images/defitalks.jpg',
-    href: 'https://open.spotify.com/episode/4u5faDs9DEPvZMDTwIRR4L?si=45844b55592f4f3a',
   },
 ]
 
