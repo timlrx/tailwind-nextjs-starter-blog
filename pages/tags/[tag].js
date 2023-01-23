2cab340dbfe5f7d42dab199a11f2cdf189b0a291
Muhammad Nasrurrohman <anas@jurnalanas.com>
2023-01-24 06:29:39 +0700

    fix: handle posts with missing tags

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -26,7 +26,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   const allPosts = await getAllFilesFrontMatter('blog')
   const filteredPosts = allPosts.filter(
-    (post) => post.draft !== true && post.tags.map((t) => kebabCase(t)).includes(params.tag)
+    (post) => post.draft !== true && (post.tags || []).map((t) => kebabCase(t)).includes(params.tag)
   )
 
   // rss
