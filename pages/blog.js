db908acc2108f7329a9707f8c92b388398b1db0f
Diar <diarselimi92+github@gmail.com>
2023-01-31 21:18:31 +0100

    fix

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,7 @@ import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
 
-export const POSTS_PER_PAGE = 5
+export const POSTS_PER_PAGE = 9
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
