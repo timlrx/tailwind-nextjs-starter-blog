12f50deeb62e8f35a56bb6913b52c192c8e7fed2
Diar <diarselimi92+github@gmail.com>
2023-02-01 21:29:13 +0100

    fix

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
