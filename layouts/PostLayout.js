aebf6e74f5eccd2d000c969bffd7f0f209274f8b
did <diarselimi92@gmail.com>
2023-02-03 09:10:27 +0100

    Fix icons

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -92,8 +92,10 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                <SocialIcon href={discussUrl(slug)} kind={'twitter'} rel="nofollow" />
-                <SocialIcon href={shareOnLinkedinUrl(slug)} kind={'linkedin'} rel="nofollow" />
+                <div className="flex space-x-3 pt-6">
+                  <SocialIcon href={discussUrl(slug)} kind={'twitter'} rel="nofollow" />
+                  <SocialIcon href={shareOnLinkedinUrl(slug)} kind={'linkedin'} rel="nofollow" />
+                </div>
               </div>
             </div>
             <footer>
