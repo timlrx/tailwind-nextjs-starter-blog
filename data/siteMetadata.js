1bc806dca7c2ebab7678d4b09b0ce190304e53e2
adel0025 <adel0025@student.monash.edu>
2023-02-15 16:37:54 +1100

    update layout

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  author: 'Aden de Leeuw',
+  headerTitle: 'ADL Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
