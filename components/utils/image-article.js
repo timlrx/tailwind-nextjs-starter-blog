a07962129bc20f2ee1751a93d6db463642f19113
Sydney Cohen <chnsydney@gmail.com>
2023-05-10 17:34:00 +0200

    user story blake

--- components/utils/image-article.js
+++ components/utils/image-article.js
@@ -1,6 +1,32 @@
 import Image from "next/image"
 import classNames from "./classNames"
 
+export const Callout = ({ emoji = "💡", title, subtitle }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#fef8f1",
+        border: "1px solid #fdd2a4",
+        padding: "20px",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ padding: "10px", marginRight: "20px", display: "inline-flex" }}>
+        <span style={{ fontSize: "42px", fontWeight: "bold" }}>{emoji}</span>
+      </div>
+      <div>
+        <p style={{ fontSize: "24px", fontWeight: "bold", margin: "0", color: "#6d4c41" }}>
+          {title}
+        </p>
+        {subtitle && (
+          <p style={{ fontSize: "16px", margin: "10px 0 0 0", color: "#8d6e63" }}>{subtitle}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
 export const ImageContainer = ({
   src,
   alt,
