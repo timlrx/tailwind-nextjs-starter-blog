27f20df0125c4a9673fa436ad0f7701ed8609716
{caoyz} <28099773+CaoYunzhou@users.noreply.github.com>
2023-05-10 20:01:54 +0800

    更新

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/blog', title: '博客' },
+  { href: '/tags', title: '标签' },
+  { href: '/projects', title: '项目' },
+  { href: '/about', title: '关于' },
 ]
 
 export default headerNavLinks
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 
-import NewsletterForm from '@/components/NewsletterForm'
+// import NewsletterForm from '@/components/NewsletterForm'
 
 const MAX_DISPLAY = 5
 
@@ -91,11 +91,11 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider !== '' && (
+      {/* {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
