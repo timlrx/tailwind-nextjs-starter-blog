1c622dc6c8fb2b2c84e46591801030ac6f177496
~zorzo <109286696+rzorzo@users.noreply.github.com>
2023-06-23 08:09:56 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
+        primary: colors.sky,
         gray: colors.neutral,
       },
       typography: (theme) => ({
