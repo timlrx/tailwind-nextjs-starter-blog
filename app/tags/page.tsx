fab1ead77b9665bc7d6918c6d67b119dc9629f46
Timothy Lin <timothy0336@hotmail.com>
2023-07-09 17:27:47 +0800

    fix: restore individual tag route

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,8 +1,7 @@
 import { getAllTags } from 'pliny/utils/contentlayer'
 import Link from '@/components/Link'
-// import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
-import { kebabCase } from 'pliny/utils/kebabCase'
+import { slug } from 'github-slugger'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 
@@ -13,7 +12,6 @@ export default async function Page() {
   const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
     <>
-      {/* <PageSEO title={`Tags - ${siteMetadata.author}`} description="Things I blog about" /> */}
       <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
         <div className="space-x-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
@@ -27,7 +25,7 @@ export default async function Page() {
               <div key={t} className="mb-2 mr-5 mt-2">
                 <Tag text={t} />
                 <Link
-                  href={`/tags/${kebabCase(t)}`}
+                  href={`/tags/${slug(t)}`}
                   className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
                   aria-label={`View posts tagged ${t}`}
                 >
