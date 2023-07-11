8d6cdf6d2a67a76a53bf888e106e07885cdecddb
Arthur Coudouy <arthur.coudouy@gmail.com>
2023-07-11 11:13:27 +0200

    in articles

--- components/userReviews/reviewCard.js
+++ components/userReviews/reviewCard.js
@@ -4,9 +4,9 @@ import ReviewStars from "./starRating"
 const ReviewCard = ({ testimonial }) => {
   return (
     // ReviewStars
-    <div className="m-4  rounded-lg bg-white p-6 shadow-md">
-      <div className="mb-4 flex items-center justify-between">
-        <div className=" flex items-center text-black">
+    <div className="m-4  rounded-lg  border border-gray-200 bg-white p-6 shadow-md dark:border-gray-600   dark:bg-gray-900">
+      <div className="mb-4 flex items-center justify-between ">
+        <div className=" flex items-center ">
           <img
             className="my-0 mr-2 h-10 w-10 rounded-full"
             src={testimonial?.image}
@@ -18,7 +18,7 @@ const ReviewCard = ({ testimonial }) => {
           <ReviewStars />
         </div>
       </div>
-      <p className="mb-4 mt-0 text-sm text-gray-700">{testimonial.text}</p>
+      <p className="mb-4 mt-0 text-sm text-gray-700 dark:text-gray-400">{testimonial.text}</p>
       <div className="flex justify-end">
         <a href="#" className="text-xs text-blue-500 hover:underline">
           Read more Axolo reviews -{">"}
