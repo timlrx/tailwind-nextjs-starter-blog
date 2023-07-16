00541391a78d97985759ea1043608d3e388933dd
云深 <yuns.xie@qq.com>
2023-07-16 11:32:54 +0800

    fix: allBlogs filter

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -7,8 +7,8 @@ import { allBlogs } from '../.contentlayer/generated/index.mjs'
 async function generate() {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
   const contentPages = allBlogs
-    .map((x) => `/${x._raw.flattenedPath}`)
     .filter((x) => !x.draft && !x.canonicalUrl)
+    .map((x) => `/${x._raw.flattenedPath}`)
   const pages = await globby([
     'pages/*.{js|tsx}',
     'public/tags/**/*.xml',
