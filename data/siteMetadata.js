fad401c50571fb8fc84876f9ad20ce6ae66815bc
Arthur Coudouy <arthur.coudouy@gmail.com>
2023-07-18 10:36:46 +0200

    ga id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -33,7 +33,7 @@ const siteMetadata = {
     plausibleDataDomain: "", // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: "", // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: "G-CYL4TTM80D", // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: process.env.GOOGLE_ANALYTICS_ID, // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
