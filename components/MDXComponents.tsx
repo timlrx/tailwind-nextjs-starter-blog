a23ba553961849045f026940c2907b495c1b04ad
Timothy Lin <timothy0336@hotmail.com>
2023-07-22 16:01:51 +0800

    fix: use correct component

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,6 @@
 import TOCInline from 'pliny/ui/TOCInline'
 import Pre from './Pre'
-import BlogNewsletterForm from 'pliny/ui/NewsletterForm'
+import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
