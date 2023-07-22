9ab992bdaa8e7b63c3d48b4c6fea0be957291cda
Timothy Lin <timothy0336@hotmail.com>
2023-07-22 16:01:25 +0800

    fix: ignore hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={siteMetadata.language} className={`${inter.className} scroll-smooth`}>
+    <html
+      lang={siteMetadata.language}
+      className={`${inter.className} scroll-smooth`}
+      suppressHydrationWarning
+    >
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
