293e16e1df8d1ca1296c2f74fc65cb2abf2d8c51
Timothy Lin <timothy0336@hotmail.com>
2023-07-23 23:06:10 +0800

    fix: use the right escape function

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,6 +1,7 @@
 import { writeFileSync, mkdirSync } from 'fs'
 import path from 'path'
 import GithubSlugger from 'github-slugger'
+import { escape } from 'pliny/utils/htmlEscaper.js'
 import siteMetadata from '../data/siteMetadata.js'
 import tagData from '../app/tag-data.json' assert { type: 'json' }
 import { allBlogs } from '../.contentlayer/generated/index.mjs'
