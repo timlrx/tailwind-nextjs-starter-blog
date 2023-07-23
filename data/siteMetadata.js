6140ff778052ef73b8302bc051a16d561cfdc038
Timothy Lin <timothy0336@hotmail.com>
2023-07-23 22:37:40 +0800

    comment out unused analytics services and fix csp

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,20 +22,20 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    plausibleAnalytics: {
-      plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    },
-    simpleAnalytics: {},
     umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
       umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
     },
-    posthogAnalytics: {
-      posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    },
-    googleAnalytics: {
-      googleAnalyticsId: '', // e.g. G-XXXXXXX
-    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
