1bbcb923331a1bbc0db03c5bae48aa2383ef3ed5
Timothy Lin <timothy0336@hotmail.com>
2023-07-23 22:26:15 +0800

    chore: remove unused components

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,5 +1,5 @@
 import TOCInline from 'pliny/ui/TOCInline'
-import Pre from './Pre'
+import Pre from 'pliny/ui/Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
