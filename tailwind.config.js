edd6b384d1b55fe211fdde1d2da85eb4d42e8372
Timothy Lin <timothy0336@hotmail.com>
2023-07-29 19:00:04 +0800

    change default font from inter to space grotesk

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['var(--font-inter)', ...fontFamily.sans],
+        sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
         primary: colors.pink,
