5ee225134456bcf04885a1139096a11aa908bfe5
Timothy Lin <timothy0336@hotmail.com>
2023-08-05 12:31:35 +0800

    add blog post for v2 release

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src 'none';
+  media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
   frame-src giscus.app
