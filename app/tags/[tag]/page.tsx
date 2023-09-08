a06efea2cb1da8354a5f7c05c56594ed3e9391f7
Lays <l4ys.tw@gmail.com>
2023-09-08 15:45:27 +0800

    chore: run prettier for tags page

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -34,8 +34,8 @@ export default function TagPage({ params }: { params: { tag: string } }) {
   const tag = decodeURI(params.tag)
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
-  const filteredPosts = allCoreContent(sortPosts(
-    allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag))
-  ))
+  const filteredPosts = allCoreContent(
+    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
+  )
   return <ListLayout posts={filteredPosts} title={title} />
 }
