92b52f60ef289d4b3b73d72656327400bd4ea5a7
vinayakg <vinayakg@live.com>
2023-09-22 01:17:40 +0530

    export option for nextjs 13

--- next.config.js
+++ next.config.js
@@ -57,8 +57,22 @@ const securityHeaders = [
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
+  output: "export"
   return plugins.reduce((acc, next) => next(acc), {
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
