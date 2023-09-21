9f50f64d3df7f4c8e77e63ef2f7e298aad9b42d9
vinayakg <vinayakg@live.com>
2023-09-22 01:51:07 +0530

    fixing output export

--- next.config.js
+++ next.config.js
@@ -54,10 +54,13 @@ const securityHeaders = [
   // },
 ]
 
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
-  output: "export"
   return plugins.reduce((acc, next) => next(acc), {
+    output: 'export',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
