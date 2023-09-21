9431608f3a8ad8dc0e0252968ec9c86ee7504bcc
vinayakg <vinayakg@live.com>
2023-09-22 01:58:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -60,7 +60,7 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
-    output: 'export',
+    // output: 'export',
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
