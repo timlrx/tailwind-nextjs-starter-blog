971302655afea55e39b3ec493691ffad379a71a7
vinayakg <vinayakg@live.com>
2023-09-22 22:08:45 +0530

    export option for nextjs 13

--- next.config.js
+++ next.config.js
@@ -54,8 +54,23 @@ const securityHeaders = [
   // },
 ]
 
+/**
+ * @type {import('next').NextConfig}
+ **/
 module.exports = {
-  output: 'export',
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
 }
 
 /**
