862f73ab739e86ee0670d6f8758e8b86d26b6346
Timothy Lin <timothy0336@hotmail.com>
2023-09-26 10:35:21 +0800

    update to next 13.5.3

--- next.config.js
+++ next.config.js
@@ -68,9 +68,6 @@ module.exports = () => {
     images: {
       domains: ['picsum.photos'],
     },
-    experimental: {
-      appDir: true,
-    },
     async headers() {
       return [
         {
