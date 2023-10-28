608b5db234074cc021593910d502470593481b9d
Billy Jacoby <billyjacoby@gmail.com>
2023-10-28 12:27:26 -0400

    fix: filter draft posts from home & blog pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,7 +8,7 @@ const POSTS_PER_PAGE = 5
 export const metadata = genPageMetadata({ title: 'Blog' })
 
 export default function BlogPage() {
-  const posts = allCoreContent(sortPosts(allBlogs))
+  const posts = allCoreContent(sortPosts(allBlogs)).filter((b) => b?.draft !== true)
   const pageNumber = 1
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
+  const sortedPosts = sortPosts(allBlogs).filter(post => post?.draft !== true)
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
 }
