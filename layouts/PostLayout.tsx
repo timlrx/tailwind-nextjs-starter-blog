5d11ab68335e345715075cb863a7c11ec40ad8ae
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-01 21:48:42 +0100

    feat: remove Twitter link
    (cherry picked from commit 0d5ef2640ecc6f679aff2a5f274425367bb2f7ef)

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -72,17 +72,6 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter.replace('https://twitter.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
                       </dl>
                     </li>
                   ))}
