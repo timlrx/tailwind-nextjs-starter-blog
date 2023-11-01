fda0bc8f976df36d6f1d4af4ca0c20d07206fade
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-01 21:47:35 +0100

    refactor: remove unused property
    (cherry picked from commit 15098298b59bcc77bad518bcfee89e4d440d86b0)

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -11,8 +11,6 @@ import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
-const discussUrl = (path) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(`${siteMetadata.siteUrl}/${path}`)}`
 
 const postDateTemplate: Intl.DateTimeFormatOptions = {
   weekday: 'long',
