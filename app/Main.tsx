f0ba2bf511c6db8d1ac2dd3ec71faf68ad9dc435
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-01 22:02:18 +0100

    feat: remove newsletter
    (cherry picked from commit 44d17603c17bd89a86a75f7f0edbd69c76ce7342)

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,7 +2,6 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const MAX_DISPLAY = 5
 
@@ -79,11 +78,6 @@ export default function Home({ posts }) {
           >
             All Posts &rarr;
           </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter?.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
         </div>
       )}
     </>
