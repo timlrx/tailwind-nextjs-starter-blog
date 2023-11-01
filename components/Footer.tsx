d619de398e4a9acf534e1bd289b3f0a4c1e2c956
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-01 21:41:35 +0100

    feat: remove email
    (cherry picked from commit e7b297610e28a992b89f6f30f5117cb0146c784c)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,6 @@ export default function Footer() {
     <footer>
       <div className="mt-16 flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
@@ -19,8 +18,6 @@ export default function Footer() {
           <SocialIcon kind="stackExchange" href={siteMetadata.stackExchange} size={6} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
