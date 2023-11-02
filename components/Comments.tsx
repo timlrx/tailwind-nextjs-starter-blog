065436637217f72ef23d165b4e0512fabf0c7cbe
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-02 23:52:32 +0100

    feat: automatically load comments
    (cherry picked from commit 051c57cab74dfb2ac63cd80f7c0fd1bd34244ce0)

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  const [loadComments, setLoadComments] = useState(true)
   return (
     <>
       {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
