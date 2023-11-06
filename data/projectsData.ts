a1ca61adba39ac72fd7a966befdf73c79a0405b5
G M <greg@gregmarlin.com>
2023-11-06 14:30:32 -0500

    updated site metadata

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,11 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'idexo',
+    description: `No-code SaaS tool and low-code API/SDK for builders of NFT innovations.`,
+    imgSrc: 'https://idexo.com/images/main-logo.svg',
+    href: 'https://idexo.com',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+ 
 ]
 
 export default projectsData
