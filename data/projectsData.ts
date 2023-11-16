0af813ea022e4754ff54cc7469c9f8e9fba55452
G M <greg@gregmarlin.com>
2023-11-16 14:53:51 -0500

    added ds app

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -5,6 +5,12 @@ const projectsData = [
     imgSrc: 'https://idexo.com/images/main-logo.svg',
     href: 'https://idexo.com',
   },
+  {
+    title: 'Deadman Switch',
+    description: `Winner of Best Mantle NFT App at EthOnline 2023 Hackathon. Anyone can post content that is hidden (encrypted) as long as they check-in on a regular basis. If they are unable to check-in the content becomes public.`,
+    imgSrc: 'https://storage.googleapis.com/ethglobal-api-production/projects%2Fs87or%2Fimages%2Fdeadmanlogin.png',
+    href: 'https://deadmanapp.com',
+  },
  
 ]
 
