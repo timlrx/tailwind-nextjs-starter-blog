fa262d04196237e9f2620b6bc49e4f5ea0bbadd8
Timothy <timothy0336@hotmail.com>
2023-11-20 23:12:38 +0800

    prettier

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,8 @@ import siteMetadata from '@/data/siteMetadata'
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl
 
-  const blogRoutes = allBlogs.filter((post) => !post.draft)
+  const blogRoutes = allBlogs
+    .filter((post) => !post.draft)
     .map((post) => ({
       url: `${siteUrl}/${post.path}`,
       lastModified: post.lastmod || post.date,
