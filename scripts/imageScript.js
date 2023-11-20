75b2fce1f66d163a79a00e578edd99038cb0cfe6
Arthur Coudouy <arthur.coudouy@gmail.com>
2023-11-20 15:24:28 +0100

    save article

--- scripts/imageScript.js
+++ scripts/imageScript.js
@@ -1,6 +1,6 @@
 // Here put the folder name and it will create a component for each image that he can
 
-const folder = "public/static/images/notifications/"
+const folder = "public/static/images/gitlab-teams/"
 
 var sizeOf = require("image-size")
 var fs = require("fs")
