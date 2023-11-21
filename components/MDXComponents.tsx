d3b744aeaf5f8d45c4cdd75df64656752472593c
PrinOrange <prinorange@outlook.com>
2023-11-21 16:00:01 +0800

    fix table element overflow

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -4,11 +4,13 @@ import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
+import TableWrapper from './TableWrapper'
 
 export const components: MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
   pre: Pre,
+  table:TableWrapper,
   BlogNewsletterForm,
 }
---
+++ components/TableWrapper.tsx
@@ -0,0 +1,7 @@
+const TableWrapper = ({children}) => {
+  return <div className="w-full overflow-x-auto">
+    {children}
+  </div>
+} 
+
+export default TableWrapper
