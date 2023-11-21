0a8e1dbbdcddc522989d878da1c727bbe6dfea90
Terminels <prinorange@outlook.com>
2023-11-21 18:17:58 +0800

    Fix the table element overflow (#787)
    * add terminals.run blog for example
    * fix table element overflow

---
+++ components/TableWrapper.tsx
@@ -0,0 +1,9 @@
+const TableWrapper = ({ children }) => {
+  return (
+    <div className="w-full overflow-x-auto">
+      <table>{children}</table>
+    </div>
+  )
+}
+
+export default TableWrapper
