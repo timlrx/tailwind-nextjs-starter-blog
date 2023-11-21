997e55e581586cfac001c4c652b5870ba17de926
PrinOrange <prinorange@outlook.com>
2023-11-21 18:05:20 +0800

    add the table tag

--- components/TableWrapper.tsx
+++ components/TableWrapper.tsx
@@ -1,7 +1,9 @@
-const TableWrapper = ({children}) => {
-  return <div className="w-full overflow-x-auto">
-    {children}
-  </div>
-} 
+const TableWrapper = ({ children }) => {
+  return (
+    <div className="w-full overflow-x-auto">
+      <table>{children}</table>
+    </div>
+  )
+}
 
 export default TableWrapper
