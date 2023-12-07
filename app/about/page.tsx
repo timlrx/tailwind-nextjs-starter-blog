003bc8f06fa2afd923d8d93068e66d8f1b130ebf
Fumiya A <jobs.fumiya@pm.me>
2023-12-07 11:30:51 +0900

    Update page.tsx
    fix `Expected component `DonutChart` to be defined`

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
+import { components } from '@/components/MDXComponents'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -13,7 +14,7 @@ export default function Page() {
   return (
     <>
       <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
+        <MDXLayoutRenderer code={author.body.code} components={components} />
       </AuthorLayout>
     </>
   )
