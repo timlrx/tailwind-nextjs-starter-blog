c33e921c2bc4708837244956232f767c449cfbbc
vinayakg <vinayakg@live.com>
2023-12-08 01:13:28 +0530

    yarn latest, Image.tsx change

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`/tailwind-nextjs-starter-blog${src}`} {...rest} />
-)
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
