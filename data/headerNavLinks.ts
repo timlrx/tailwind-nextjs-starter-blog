2997064e3a4616947770de42739f386691a15683
Stephon Parker <sgparker62@gmail.com>
2024-01-01 13:21:04 -0500

    adding site articles and personalization

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
