c5e1a20ee67cf866cad99ac6712a1b3b43235b04
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-01-15 17:57:24 -0500

    updating site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/jboliv01/next-js-blog',
   siteLogo: '/static/images/code.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'jboliv01@gmail.com',
