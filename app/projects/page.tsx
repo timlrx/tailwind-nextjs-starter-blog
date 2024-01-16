a3147a4842a96238dfbbb07ecf6c8fb84a3ed841
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-01-16 14:49:35 -0500

    update projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,6 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
           </p>
         </div>
         <div className="container py-12">
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
+    title: 'Coming Soon',
+    description: `Projects coming soon!`,
+    imgSrc: '/static/images/arch_2.png',
     href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
