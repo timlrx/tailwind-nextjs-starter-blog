fa4ee5c08c6a7f8de4f99aca66df6f64c5b1bc1b
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-01-15 21:09:17 -0500

    updating blog and changing email provider to emailoctopus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -37,7 +37,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: 'emailoctopus',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
