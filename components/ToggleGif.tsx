372a6d091331bcd91f1c248ab66d61fd4fcb876d
jboliv01 <57547273+jboliv01@users.noreply.github.com>
2024-01-26 14:58:01 -0500

    prettify ToggleGif.tsx

--- components/ToggleGif.tsx
+++ components/ToggleGif.tsx
@@ -1,45 +1,43 @@
 'use client'
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import { PlayCircleIcon, PlayIcon, PlayPauseIcon } from '@heroicons/react/24/solid';
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { PlayCircleIcon, PlayIcon, PlayPauseIcon } from '@heroicons/react/24/solid'
 
 const ToggleGif = ({ gifSrc, staticSrc, alt }) => {
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false)
 
   const toggleGif = () => {
-    setIsPlaying(!isPlaying);
-  };
+    setIsPlaying(!isPlaying)
+  }
 
   return (
-    <div 
-      onClick={toggleGif} 
-      className="relative w-full h-full cursor-pointer"
+    <div
+      onClick={toggleGif}
+      className="relative h-full w-full cursor-pointer"
       onMouseEnter={() => !isPlaying && setIsPlaying(false)}
     >
       {!isPlaying ? (
         <>
-          <Image src={staticSrc} alt={alt} layout="responsive" width={16} height={9}/>
-          <PlayCircleIcon
-            className="absolute top-1/2 left-1/2 w-12 h-12 text-teal-500 opacity-75 transform -translate-x-1/2 -translate-y-1/2"
-          />
+          <Image src={staticSrc} alt={alt} layout="responsive" width={16} height={9} />
+          <PlayCircleIcon className="absolute left-1/2 top-1/2 h-12 w-12 -translate-x-1/2 -translate-y-1/2 transform text-teal-500 opacity-75" />
         </>
-      ) : ( 
-        <img src={gifSrc} alt={alt} className="w-full h-full" />
+      ) : (
+        <img src={gifSrc} alt={alt} className="h-full w-full" />
       )}
-    </div> 
-  );
-};
+    </div>
+  )
+}
 
 const ClientToggleGif = (props) => {
-  const [isClient, setIsClient] = useState(false);
+  const [isClient, setIsClient] = useState(false)
 
   useEffect(() => {
     // Set isClient to true once we know we're in the browser
-    setIsClient(true);
-  }, []);
+    setIsClient(true)
+  }, [])
 
   // Only render the ToggleGif component on the client-side
-  return isClient ? <ToggleGif {...props} /> : null;
-};
+  return isClient ? <ToggleGif {...props} /> : null
+}
 
-export default ClientToggleGif;
+export default ClientToggleGif
