92812a47db30feb62dd905797fef2a75f54f5a39
James Acres <jamesacres@gmail.com>
2024-01-28 18:35:24 +0000

    adjust speech bubble animation

--- components/SpeechBubble.tsx
+++ components/SpeechBubble.tsx
@@ -5,11 +5,14 @@ import React from 'react';
 export default function SpeechBubble({ children }) {
   const [isVisible, setIsVisible] = React.useState(false);
   React.useEffect(() => {
-    setIsVisible(true);
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 250);
   }, []);
   return (
     <div
-      className={`flex items-center justify-end opacity-${isVisible ? 100 : 0} transition-opacity duration-500 ease-in`}
+      className={`flex items-center justify-end transition-opacity duration-500 ease-in`}
+      style={{ opacity: isVisible ? undefined : 0 }}
     >
       <div
         className="my-6 flex-1 rounded-lg bg-pink-500 p-4"
