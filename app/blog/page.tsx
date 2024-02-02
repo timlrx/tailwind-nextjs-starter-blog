3b8441d5929d4e4ac3b4e5da3c340c3fbeb87075
conglytran0301 <congly.work@gmail.com>
2024-02-02 12:52:46 +0700

    Version-1.5.2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,5 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
+import projectsData from '@/data/blogData'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
