3d9d4347d6f734583f30a60a79a37720ee9bd4bc
conglytran0301 <congly.work@gmail.com>
2024-02-02 13:01:44 +0700

    Version-1.5.3

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,4 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
-import projectsData from '@/data/blogData'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -15,12 +15,10 @@ const projectsData: Series[] = [
     href: 'https://www.google.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Amazon Web Service',
+    description: `Các bài viết về AWS`,
+    imgSrc: '/static/images/derivong-ols.jpg',
+    href: '/blog/nested-route',
   },
 ]
 
