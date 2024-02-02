67318994712be3e58d3389eb6dee626fdf739a98
conglytran0301 <congly.work@gmail.com>
2024-02-02 19:36:35 +0700

    Version-1.6.1

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,11 +23,11 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        {/*<div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
           </Link>
-        </div>
+         </div>*/}
       </div>
     </footer>
   )
