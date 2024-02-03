47f1d410e402f3973e32496b112eae0674b8992a
conglytran0301 <congly.work@gmail.com>
2024-02-03 14:21:36 +0700

    Version-1.7.4

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -24,7 +24,7 @@ export default function BlogPage() {
       posts={posts}
       initialDisplayPosts={initialDisplayPosts}
       pagination={pagination}
-      title="All Posts" 
+      title="All Posts"
     />
   )
 }
