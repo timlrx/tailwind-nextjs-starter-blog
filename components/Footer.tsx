65fc95184aabb20c98ead21be84b9d3f3b070503
conglytran0301 <congly.work@gmail.com>
2024-02-03 11:43:00 +0700

    Version-1.6.6

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,11 +10,11 @@ export default function Footer() {
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-         {/*<SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />*/}
+          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
           <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
-         {/*<SocialIcon kind="threads" href={siteMetadata.threads} size={6} />*/}
+          <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
