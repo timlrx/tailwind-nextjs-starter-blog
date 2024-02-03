c32ce81765271510e22b781ffdab17f6f3f9e76d
talhatahir <trinitro619@gmail.com>
2024-02-03 18:58:46 +0500

    fix: update comments component incase of no provider

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,13 +5,16 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  const [loadComments, setLoadComments] = useState(false);
+  
+  if (!siteMetadata.comments?.provider) {
+    return null;
+  }
   return (
-    <>
-      {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
-      {siteMetadata.comments && loadComments && (
+      {loadComments ? (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      )}
-    </>
+      ) :
+        (<button onClick={() => setLoadComments(true)}>Load Comments</button>)
+      }
   )
 }
