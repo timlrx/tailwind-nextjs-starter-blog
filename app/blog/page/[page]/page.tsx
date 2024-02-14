ecbe8a5691c61b7c69878fbd342dcdec7190268b
conglytran0301 <congly.work@gmail.com>
2024-02-14 11:26:12 +0700

    1.0.3

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -26,18 +26,12 @@ export default function Page({ params }: { params: { page: string } }) {
   }
 
   return (
-    <><ListLayout
-      posts={posts}
-      initialDisplayPosts={initialDisplayPosts}
-      pagination={pagination}
-      title="All Posts" /><>
+    <>
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
           <div className="space-y-2 pb-8 pt-6 md:space-y-5">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Blog
             </h1>
             <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-              Tổng hợp các blog
             </p>
           </div>
           <div className="container py-12">
