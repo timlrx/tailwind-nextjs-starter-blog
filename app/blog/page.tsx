536882c749d71b8efef96a6b282b42de907fda9d
conglytran0301 <congly.work@gmail.com>
2024-02-14 11:31:29 +0700

    1.0.4

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,4 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
-import blogData from '@/data/blogData'
-import Card from '@/components/Card'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
@@ -22,27 +20,11 @@ export default function BlogPage() {
   }
 
   return (
-    <>
-        <div className="divide-y divide-gray-200 dark:divide-gray-700">
-          <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            </h1>
-            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            </p>
-          </div>
-          <div className="container py-12">
-            <div className="-m-4 flex flex-wrap">
-              {blogData.map((d) => (
-                <Card
-                  key={d.title}
-                  title={d.title}
-                  description={d.description}
-                  imgSrc={d.imgSrc}
-                  href={d.href} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </></>
+    <ListLayout
+      posts={posts}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      title="All Posts"
+    />
   )
 }
