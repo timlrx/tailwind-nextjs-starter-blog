22013a1773bf05218ec4939cba98c1364eb17fe5
conglytran0301 <congly.work@gmail.com>
2024-02-14 11:05:18 +0700

    1.0.1

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,5 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
+import Card from '@/components/Card'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
@@ -20,11 +21,33 @@ export default function BlogPage() {
   }
 
   return (
-    <ListLayout
+    <><ListLayout
       posts={posts}
       initialDisplayPosts={initialDisplayPosts}
       pagination={pagination}
-      title="All Posts"
-    />
+      title="All Posts" /><>
+        <div className="divide-y divide-gray-200 dark:divide-gray-700">
+          <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              Series
+            </h1>
+            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+              Tổng hợp các blog
+            </p>
+          </div>
+          <div className="container py-12">
+            <div className="-m-4 flex flex-wrap">
+              {projectsData.map((d) => (
+                <Card
+                  key={d.title}
+                  title={d.title}
+                  description={d.description}
+                  imgSrc={d.imgSrc}
+                  href={d.href} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </></>
   )
 }
