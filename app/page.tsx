c9c85838cdea384e2155e702be86c3a18d1592e1
hans <im.hansking@gmail.com>
2024-03-20 15:05:20 +0800

    no message

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,31 @@ export default function Home() {
   return (
     <>
       <Greetings />
+      {/*  */}
+      <br />
+      Yi NextChat 🤸
+      <iframe
+        title="agent"
+        src="https://nextchat.hansking.cn/"
+        height="500px"
+        width="100%"
+      ></iframe>
+      <br />
+      Hans Gallery 🌇
+      <iframe
+        title="agent"
+        src="https://gallery.hansking.cn/grid"
+        height="500px"
+        width="100%"
+      ></iframe>
+      <br />
+      <a target="_blank" href="https://github.com/HansKing98/bob-plugin-yi-translate">
+        Yi Translator for Bob 👽
+      </a>
+      <img
+        src="http://image.hansking.cn/picgo/%E6%88%AA%E5%B1%8F2024-03-20%2015.02.20.png"
+        alt=""
+      />
     </>
   )
 }
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   // { href: '/about', title: 'About' },
+  { href: 'https://nextchat.hansking.cn', title: 'NextChat' },
   { href: 'https://gallery.hansking.cn', title: 'Gallery' },
   { href: '/blog', title: 'Blog' },
   { href: '/plog', title: 'Plog' },
