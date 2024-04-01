948604b4c7aa1a0c31ab7b8683ead92126f60c59
Talha Tahir <trinitro619@gmail.com>
2024-04-01 14:29:08 +0500

    fix: lint error

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,16 +5,18 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false);
-  
+  const [loadComments, setLoadComments] = useState(false)
+
   if (!siteMetadata.comments?.provider) {
-    return null;
+    return null
   }
   return (
+    <>
       {loadComments ? (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) :
-        (<button onClick={() => setLoadComments(true)}>Load Comments</button>)
-      }
+      ) : (
+        <button onClick={() => setLoadComments(true)}>Load Comments</button>
+      )}
+    </>
   )
 }
