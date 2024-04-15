80bd852c02481b4a85dc69e2ef1e206b4414ce44
akbiv <dipshikhaadhikari96@gmail.com>
2024-04-15 13:24:56 +0545

    Make fixed position for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,39 +8,41 @@ import SearchButton from './SearchButton'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between py-10">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
+    <header className="fixed left-0 right-0  bg-white  dark:bg-gray-950">
+      <section className="mx-auto flex h-10 w-full max-w-[1200px] items-center justify-between px-10 py-10">
+        <div>
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              <div className="mr-3">
+                <Logo />
               </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
-      </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
-      </div>
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
+              >
+                {link.title}
+              </Link>
+            ))}
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </section>
     </header>
   )
 }
