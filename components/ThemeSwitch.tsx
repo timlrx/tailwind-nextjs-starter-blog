edce2438b58322ff9f03cb2df71392edb679594d
PxlSyl <79746927+PxlSyl@users.noreply.github.com>
2024-04-30 21:28:26 +0200

    Fix z-index in ThemeSwitch.tsx

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -67,7 +67,7 @@ const ThemeSwitch = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800">
+          <Menu.Items className="absolute right-0 z-50 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800">
             <RadioGroup value={theme} onChange={setTheme}>
               <div className="p-1">
                 <RadioGroup.Option value="light">
