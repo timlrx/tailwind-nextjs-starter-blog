194d7072d46564ca3ad7934ab9dce4348f25ce06
Timothy Lin <timothy0336@hotmail.com>
2024-05-01 11:09:29 +0800

    add x logo

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -37,7 +37,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
+              <SocialIcon kind="x" href={twitter} />
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
