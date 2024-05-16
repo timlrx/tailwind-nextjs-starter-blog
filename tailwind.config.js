691c1208b837a089a406eed9df4f7a4e889c77bc
Tony <geisera@gmail.com>
2024-05-16 12:31:31 -0500

    New favicon New link color: red

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.pink,
+        primary: colors.red,
         gray: colors.gray,
       },
       typography: ({ theme }) => ({
