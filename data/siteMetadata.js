97132706f914f77cb78ceb54cadcfa1c4745c4ce
rdg1029 <wcle3456@gmail.com>
2024-05-17 11:34:56 +0900

    Update siteMetadata.js : Disable comments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -48,36 +48,36 @@ const siteMetadata = {
     // Please add your .env file and modify it according to your selection
     provider: 'buttondown',
   },
-  comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
-    giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      // theme when dark mode
-      darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'kr',
-    },
-  },
+  // comments: {
+  //   // If you want to use an analytics provider you have to add it to the
+  //   // content security policy in the `next.config.js` file.
+  //   // Select a provider and use the environment variables associated to it
+  //   // https://vercel.com/docs/environment-variables
+  //   provider: 'giscus', // supported providers: giscus, utterances, disqus
+  //   giscusConfig: {
+  //     // Visit the link below, and follow the steps in the 'configuration' section
+  //     // https://giscus.app/
+  //     repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+  //     repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+  //     category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+  //     categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+  //     mapping: 'pathname', // supported options: pathname, url, title
+  //     reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+  //     // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+  //     metadata: '0',
+  //     // theme example: light, dark, dark_dimmed, dark_high_contrast
+  //     // transparent_dark, preferred_color_scheme, custom
+  //     theme: 'light',
+  //     // theme when dark mode
+  //     darkTheme: 'transparent_dark',
+  //     // If the theme option above is set to 'custom`
+  //     // please provide a link below to your custom theme css file.
+  //     // example: https://giscus.app/themes/custom_example.css
+  //     themeURL: '',
+  //     // This corresponds to the `data-lang="en"` in giscus's configurations
+  //     lang: 'kr',
+  //   },
+  // },
   search: {
     provider: 'kbar', // kbar or algolia
     kbarConfig: {
