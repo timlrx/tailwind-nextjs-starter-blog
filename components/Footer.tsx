64e30a9f1f4651ff74e330d5183908740dc5c99d
rdg1029 <wcle3456@gmail.com>
2024-05-17 09:08:11 +0900

    Update Footer.tsx : Edit social icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
         <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+          {/* <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} /> */}
+          {/* <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} /> */}
+          {/* <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} /> */}
+          {/* <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
           <SocialIcon kind="x" href={siteMetadata.x} size={6} />
           <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
-          <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
+          <SocialIcon kind="threads" href={siteMetadata.threads} size={6} /> */}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
