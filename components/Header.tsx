1735af388ade2fa53009fee084cea1a99c2294aa
Nam Thang <thangvynam1808@gmail.com>
2024-05-18 21:07:39 +0700

    feat: add cv

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -12,8 +11,6 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
-            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -39,6 +39,13 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
             </div>
+            <a
+              className="pb-2 pt-4 text-3xl font-bold leading-8 tracking-tight"
+              href="/static/images/blog/Nam-Thang-CV.pdf"
+              download="Nam-Thang-CV.pdf"
+            >
+              Download CV
+            </a>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
             {children}
