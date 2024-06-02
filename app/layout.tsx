42cce1d8967d4ee8dc3bab421dbe2b92746146fb
Timothy Lin <timothy0336@hotmail.com>
2024-06-02 22:05:25 +0800

    add support for github alerts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
+import 'remark-github-blockquote-alert/alert.css'
 
 import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
