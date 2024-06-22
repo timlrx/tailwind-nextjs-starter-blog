88a7710cb0306f0926a8ace586c9742b1085dcbf
Trillium Smith <spiteless@gmail.com>
2024-06-22 01:45:12 -0700

    feat: Remove headerNavLinks from Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,4 @@
 import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
@@ -27,17 +26,7 @@ const Header = () => {
         </Link>
       </div>
       <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
-            >
-              {link.title}
-            </Link>
-          ))}
+        <NavOptions />
         <SearchButton />
         <ThemeSwitch />
         <MobileNav />
