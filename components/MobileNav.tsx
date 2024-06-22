b301981712f5f522abb13ccc3bdd7289de45e304
Trillium Smith <spiteless@gmail.com>
2024-06-22 01:48:24 -0700

    feat: Render links with heperFunc

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -64,17 +64,15 @@ const MobileNav = () => {
               >
                 <Dialog.Panel className="fixed left-0 top-0 z-10 h-full w-full bg-white opacity-95 duration-300 dark:bg-gray-950 dark:opacity-[0.98]">
                   <nav className="fixed mt-8 h-full text-left">
-                    {headerNavLinks.map((link) => (
-                      <div key={link.title} className="px-12 py-4">
-                        <Link
-                          href={link.href}
-                          className="text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100"
-                          onClick={onToggleNav}
-                        >
-                          {link.title}
-                        </Link>
-                      </div>
-                    ))}
+                    {headerNavLinks.map((link) => {
+                      return (
+                        <RenderMobileNavLink
+                          navLink={link}
+                          clickFunc={onToggleNav}
+                          key={link.title}
+                        />
+                      )
+                    })}
                   </nav>
 
                   <div className="flex justify-end">
