f4476ba55f762e41c8ca950165e0936549479553
Timothy <timothy0336@hotmail.com>
2024-06-23 17:45:11 +0800

    Fix email domain validation

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -31,7 +31,10 @@ type SocialIconProps = {
 }
 
 const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
-  if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
+  if (
+    !href ||
+    (kind === 'mail' && !/^mailto:[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(href))
+  )
     return null
 
   const SocialSvg = components[kind]
