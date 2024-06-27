5ceda010c686a9502e4003f74c50a9ec9cfe342b
Aaro_laptop <aaro.heroja@gmail.com>
2024-06-27 11:14:26 +0300

    feat: added notice that site isn't ready

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,6 +11,9 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-center text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl md:leading-14">
+            Moi! Sivusto ei ole vielä valmis.
+          </h1>
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Viimeisimmät
           </h1>
