dc7157d3dbba219c60f4af5a19f7eeb7c457a10f
Sydney Cohen <chnsydney@gmail.com>
2024-07-02 14:06:05 +0200

    git commit msg

--- scripts/imageScript.js
+++ scripts/imageScript.js
@@ -1,6 +1,6 @@
 // Here put the folder name and it will create a component for each image that he can
 
-const folder = "public/static/images/slack-statistics/"
+const folder = "public/static/images/commitmessages/"
 
 var sizeOf = require("image-size")
 var fs = require("fs")
