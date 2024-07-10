09b2a9ca0638f4a2df066074b9a92bf1f96619da
Antoine BERNIER <antoine.bernier@gmail.com>
2024-07-10 23:08:39 +0200

    searchDocumentsPath taking BASE_PATH into account

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -83,7 +83,7 @@ function createSearchIndex(allBlogs) {
     siteMetadata.search.kbarConfig.searchDocumentsPath
   ) {
     writeFileSync(
-      `public/${siteMetadata.search.kbarConfig.searchDocumentsPath}`,
+      `public/${path.basename(siteMetadata.search.kbarConfig.searchDocumentsPath)}`,
       JSON.stringify(allCoreContent(sortPosts(allBlogs)))
     )
     console.log('Local search index generated...')
