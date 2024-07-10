c1235a3f8b2b0a5d944f0abb9c40a85d9d574837
Timothy <timothy0336@hotmail.com>
2024-07-10 21:28:18 +0800

    Run prettier on theme switch

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -54,7 +54,7 @@ const ThemeSwitch = () => {
   useEffect(() => setMounted(true), [])
 
   return (
-    <div className="flex items-center mr-5">
+    <div className="mr-5 flex items-center">
       <Menu as="div" className="relative inline-block text-left">
         <div className="flex items-center justify-center hover:text-primary-500 dark:hover:text-primary-400">
           <Menu.Button>
