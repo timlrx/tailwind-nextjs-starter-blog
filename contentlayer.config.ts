5e01d7599ceda982ff1d226d4b853b231fbffd25
Antoine BERNIER <antoine.bernier@gmail.com>
2024-07-14 11:11:45 +0200

    rehypeMermaid

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -23,6 +23,7 @@ import rehypePrismPlus from 'rehype-prism-plus'
 import rehypePresetMinify from 'rehype-preset-minify'
 import siteMetadata from './data/siteMetadata'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer.js'
+import rehypeMermaid from 'rehype-mermaid'
 
 const root = process.cwd()
 const isProduction = process.env.NODE_ENV === 'production'
@@ -157,6 +158,7 @@ export default makeSource({
       remarkAlert,
     ],
     rehypePlugins: [
+      rehypeMermaid,
       rehypeSlug,
       [
         rehypeAutolinkHeadings,
