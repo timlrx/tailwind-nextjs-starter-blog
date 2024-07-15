33eca80cb6fc314cf78edaf5d6880f0686ad9f87
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-07-15 00:25:49 -0700

    Change H1 style

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -1,6 +1,6 @@
 export function H1({ children }: { children?: React.ReactNode }) {
   return (
-    <h1 className="text-4xl uppercase leading-none tracking-tight sm:text-5xl md:text-7xl">
+    <h1 className="text-4xl font-semibold leading-none tracking-tight sm:text-5xl md:text-6xl">
       {children}
     </h1>
   )
