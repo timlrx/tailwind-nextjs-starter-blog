49e1376c271bf6fdde4f9ce7d9b20ad6d0843623
Peter Krauß <pk@mbp-von-peter.speedport.ip>
2024-07-21 12:34:17 +0200

    remove old articles and newsletter

--- app/Main.tsx
+++ app/Main.tsx
@@ -81,11 +81,11 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
