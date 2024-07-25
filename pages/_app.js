86d30053a14a6247b258d96ff5c27b8a2f763085
Sydney Cohen <chnsydney@gmail.com>
2024-07-25 10:04:29 +0200

    gtag blog'

--- pages/_app.js
+++ pages/_app.js
@@ -84,8 +84,28 @@ export default function App({ Component, pageProps }) {
               }}
               defer
             />
+            {/* Google Tag Manager */} vv
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MCPBKZDL');`,
+              }}
+            />
+            {/* End Google Tag Manager */}
           </Head>
           {process.env.NODE_ENV === "production" && <GoogleAnalytics trackPageViews />}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MCPBKZDL"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
 
           {isDevelopment && isSocket && <ClientReload />}
           <Analytics />
