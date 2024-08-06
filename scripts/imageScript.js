e804dab6df07eb29c6b9586f6795818005c9ae5e
Sydney Cohen <chnsydney@gmail.com>
2024-08-06 17:24:41 +0200

    incidents

--- scripts/imageScript.js
+++ scripts/imageScript.js
@@ -1,6 +1,6 @@
 // Here put the folder name and it will create a component for each image that he can
 
-const folder = "public/static/images/commitmessages/"
+const folder = "public/static/images/incident-report-template/"
 
 var sizeOf = require("image-size")
 var fs = require("fs")
