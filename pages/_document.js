79f1cc90c21fdc6f90e9993ee45c7c2d57076722
Sydney Cohen <chnsydney@gmail.com>
2024-08-15 11:41:04 +0200

    "fix html errors"

--- pages/_document.js
+++ pages/_document.js
@@ -29,7 +29,7 @@ class MyDocument extends Document {
           />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
-          <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <link rel="alternate" type="application/rss+xml" href="/feed.xml" itemProp="url" />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
