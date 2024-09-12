717fe4aee3cf174cf741ad1f0982a1251265b0e8
Timothy Lin <timothy0336@hotmail.com>
2024-09-12 21:59:17 +0800

    fix toc wrong typing

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -54,7 +54,7 @@ const computedFields: ComputedFields = {
     type: 'string',
     resolve: (doc) => doc._raw.sourceFilePath,
   },
-  toc: { type: 'string', resolve: (doc) => extractTocHeadings(doc.body.raw) },
+  toc: { type: 'json', resolve: (doc) => extractTocHeadings(doc.body.raw) },
 }
 
 /**
