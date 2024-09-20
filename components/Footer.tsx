2d60e6227165a2a81300d956ba0a2e571005a5c8
Bora Lee <learnerbr@gmail.com>
2024-09-20 18:52:18 +0900

    Add medium to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,6 +16,7 @@ export default function Footer() {
           <SocialIcon kind="x" href={siteMetadata.x} size={6} />
           <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
+          <SocialIcon kind="medium" href={siteMetadata.medium} size={6} />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,6 +20,7 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com',
   threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com',
+  medium: 'https://medium.com',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
