d488b400530d525fb3cffe1483271470bdb6cfba
Hai.Nguyen <haingdc@gmail.com>
2024-09-23 19:24:04 +0700

    create SocialIcon component for social media links and refactor icons; add zigzag background utility in Tailwind CSS

---
+++ components/social-icons/social-icon-components.ts
@@ -0,0 +1,25 @@
+import {
+  Mail,
+  Github,
+  Facebook,
+  Youtube,
+  Linkedin,
+  Twitter,
+  X,
+  Mastodon,
+  Threads,
+  Instagram,
+} from './icons'
+
+export const components = {
+  mail: Mail,
+  github: Github,
+  facebook: Facebook,
+  youtube: Youtube,
+  linkedin: Linkedin,
+  twitter: Twitter,
+  x: X,
+  mastodon: Mastodon,
+  threads: Threads,
+  instagram: Instagram,
+}
