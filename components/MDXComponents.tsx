3c1d4da4fcf598fdb5d243c94a34cf096c3a4e22
Hai.Nguyen <haingdc@gmail.com>
2024-10-13 14:10:31 +0700

    fix: update import path for coffee-time-components and add new index file for component exports

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -8,7 +8,7 @@ import TOCInline from 'pliny/ui/TOCInline'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
-import { Details, Summary, Content } from 'coffee-time-components'
+import { Details, Summary, Content } from './coffee-time-components'
 
 export const components: MDXComponents = {
   Image,
