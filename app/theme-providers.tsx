956d36471ad77ac177149a57b63a90d01180b9ca
tidvn <tiendung0325@gmail.com>
2024-10-14 01:29:31 +0700

    edit theme

--- app/theme-providers.tsx
+++ app/theme-providers.tsx
@@ -5,7 +5,11 @@ import siteMetadata from '@/data/siteMetadata'
 
 export function ThemeProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme} enableSystem>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme={siteMetadata.theme}
+      enableSystem={siteMetadata.theme === 'system'}
+    >
       {children}
     </ThemeProvider>
   )
