85fe09b13e78e71e21fdc4972d02e54d1a843f7d
Timothy Lin <timothy0336@hotmail.com>
2024-10-31 22:37:40 +0800

    apply async codemod

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -11,7 +11,8 @@ export const generateStaticParams = async () => {
   return paths
 }
 
-export default function Page({ params }: { params: { page: string } }) {
+export default async function Page(props: { params: Promise<{ page: string }> }) {
+  const params = await props.params
   const posts = allCoreContent(sortPosts(allBlogs))
   const pageNumber = parseInt(params.page as string)
   const initialDisplayPosts = posts.slice(
