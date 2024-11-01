d46e7f638789a4c1508011e34dbe7626920e903f
PrinOrange <prinorange@outlook.com>
2024-11-01 17:14:23 +0800

    fix(format): format the code

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -47,9 +47,7 @@ async function generateRSS(config, allBlogs, page = 'feed.xml') {
 
   if (publishPosts.length > 0) {
     for (const tag of Object.keys(tagData)) {
-      const filteredPosts = allBlogs.filter((post) =>
-        post.tags.map((t) => slug(t)).includes(tag)
-      )
+      const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => slug(t)).includes(tag))
       const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
       const rssPath = path.join(outputFolder, 'tags', tag)
       mkdirSync(rssPath, { recursive: true })
