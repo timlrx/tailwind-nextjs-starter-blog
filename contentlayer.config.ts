6289f94e8737ce225071769472f7e8a3631fdc58
秋刀鱼之愿 <prinorange@outlook.com>
2024-11-03 17:01:35 +0800

    feat: add `rehype-katex-notranslate` plugin for preventing symbols in formulas to be translated by browser translate-tools.  (#1049)
    * fix(format): format the codes
    
    * fix(format): format the code
    
    * feat: add rehype-katex-notranslate plugin to prevent symbol in formulas to be translated.
    
    * update(dep): update `rehype-katex-notranslate` version

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -18,6 +18,7 @@ import {
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeKatex from 'rehype-katex'
+import rehypeKatexNoTranslate from 'rehype-katex-notranslate'
 import rehypeCitation from 'rehype-citation'
 import rehypePrismPlus from 'rehype-prism-plus'
 import rehypePresetMinify from 'rehype-preset-minify'
@@ -169,6 +170,7 @@ export default makeSource({
         },
       ],
       rehypeKatex,
+      rehypeKatexNoTranslate,
       [rehypeCitation, { path: path.join(root, 'data') }],
       [rehypePrismPlus, { defaultLanguage: 'js', ignoreMissing: true }],
       rehypePresetMinify,
