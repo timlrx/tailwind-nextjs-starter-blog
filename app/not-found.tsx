07721066ee1500d816184627f223bbd2fa1a99ef
Rifki Andriyanto <andriyantorifki@gmail.com>
2024-11-04 21:00:24 +0700

    fix: responsive not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,20 +2,22 @@ import Link from '@/components/Link'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
+    <div className="mt-12 flex flex-col items-center justify-center space-y-8 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+      <div className="pb-8 pt-6 text-center md:text-left">
+        <h1 className="text-5xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-gray-100 sm:text-6xl md:border-r-2 md:px-6 md:text-8xl md:leading-14">
           404
         </h1>
       </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+      <div className="max-w-md text-center md:text-left">
+        <p className="mb-4 text-lg font-bold leading-normal text-gray-700 dark:text-gray-300 sm:text-xl md:text-2xl">
           Sorry we couldn't find this page.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8 text-gray-600 dark:text-gray-400">
+          But don’t worry, you can find plenty of other things on our homepage.
+        </p>
         <Link
           href="/"
-          className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+          className="focus:shadow-outline-blue inline-block rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
         >
           Back to homepage
         </Link>
