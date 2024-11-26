df3ea70a7f8961dc67780c68886d248d9e7539f9
Arthur Coudouy <arthur.coudouy@gmail.com>
2024-11-26 09:05:28 +0100

    save

--- scripts/imageScript.js
+++ scripts/imageScript.js
@@ -1,6 +1,6 @@
 // Here put the folder name and it will create a component for each image that he can
 
-const folder = "public/static/images/clean-code/"
+const folder = "public/static/images/branching/"
 
 var sizeOf = require("image-size")
 var fs = require("fs")
