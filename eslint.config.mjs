747ac6eb65ba0fc1f501e544a300ef8c28c52732
tqt97 <kutuanonline199@gmail.com>
2024-12-13 21:38:33 +0700

    main

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -65,6 +65,7 @@ export default [
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-var-requires': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
+      "@typescript-eslint/no-require-imports": "off",
     },
   },
 ]
