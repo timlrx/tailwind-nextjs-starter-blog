2b931f5eea5ec17f3ef36cce7720ae5085428fbc
erickleite338 <erickleite338@gmail.com>
2024-12-24 13:17:28 +0000

    feat(page): Add pagination and initial number of posts on the main page
    - Added constant `POSTS_PER_PAGE` to define the number of posts per page
    (5)
    - Implemented pagination logic in the `Page` component
    - Added variables `pageNumber`, `initialDisplayPosts` and `pagination`
    - Updated the `Main` component to receive `posts`, `initialDisplayPosts`
    and `pagination`

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,20 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
+const POSTS_PER_PAGE = 5
+
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  const pageNumber = 1
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+
+  return <Main posts={posts} initialDisplayPosts={initialDisplayPosts} pagination={pagination} />
 }
