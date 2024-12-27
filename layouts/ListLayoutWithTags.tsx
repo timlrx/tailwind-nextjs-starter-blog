0e8356f8c6ee51193015e46789114a51bfa9d2f7
Trillium Smith <spiteless@gmail.com>
2024-12-27 11:42:19 -0800

    chore: Remove unused imports from ListLayoutWithTags

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -2,16 +2,11 @@
 
 import { usePathname } from 'next/navigation'
 import { slug } from 'github-slugger'
-import { formatDate } from 'pliny/utils/formatDate'
 import { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from 'contentlayer/generated'
 import Link from '@/components/Link'
-import Tag from '@/components/Tag'
-import siteMetadata from '@/data/siteMetadata'
 import tagData from 'app/tag-data.json'
-import clsx from 'clsx'
 import type { PaginationProps } from '@/components/Pagination'
-import Pagination from '@/components/Pagination'
 import ListLayoutBase from '@/components/ListLayoutBase'
 
 export interface ListLayoutProps {
