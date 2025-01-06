088754f90ff07b2d61dcc48326c6c70eb6db3b68
maxiaoteng <jizhuwo00@hotmail.com>
2025-01-06 15:42:57 +0800

    feat(blog): refactor blog pagination and remove deprecated page component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,18 +2,24 @@ import ListLayout from '@/layouts/ListLayoutWithTags'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
+import { notFound } from 'next/navigation'
 
 const POSTS_PER_PAGE = 5
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
-export default function BlogPage() {
+export default async function BlogPage(props: { searchParams: Promise<{ page: string }> }) {
   const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
+  const searchParams = await props.searchParams
+  const pageNumber = parseInt(searchParams.page || '1')
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
     POSTS_PER_PAGE * pageNumber
   )
+  if (initialDisplayPosts.length === 0) {
+    return notFound()
+  }
+
   const pagination = {
     currentPage: pageNumber,
     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
