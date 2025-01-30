dc5ffd78f980a8bbf98186768f9e9874491f04f0
erickleite338 <erickleite338@gmail.com>
2025-01-30 14:10:23 +0000

    feat(Main): Add new CoverImage properties and Image component
    - Added `Image` component from 'next/image' to handle cover images in
    `Home` component
    - Updated `displayPosts` mapping to handle `coverImage` properties,
    including `url`, `alt`, `width`, `height`, and `home`
    - Included conditional rendering for images based on `coverImage`
    properties
    - Enhanced layout with additional class names and styles for images
    - Refined structure for `dl` and `div` elements to support new image
    handling
    - Replace postImage component with Image

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,9 +1,9 @@
+import Image from 'next/image'
 import { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from 'contentlayer/generated'
 import type { PaginationProps } from '@/components/Pagination'
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
-import PostImage from '@/components/PostImage'
 import Pagination from '@/components/Pagination'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
@@ -37,37 +37,51 @@ export default function Home({ posts, pagination, initialDisplayPosts = [] }: Ho
         <ul className="divide-y divide-gray-200 dark:divide-gray-700 md:divide-y-0">
           {!posts.length && 'No posts found.'}
           {displayPosts.map((post, index) => {
-            const { path, slug, date, images, title, summary, tags } = post
-            const postImageSrc = images && images[0] ? images[0] : null
+            const { path, slug, date, coverImage, title, summary, tags } = post
+            const { url, alt, width, height, home } = coverImage ?? {}
+            const { parentClass, childClass, parentStyle, childStyle } = home ?? {}
+            const isSquare = width && height ? width === height : false
+            const isVertical = width && height ? width < height : false
+            const hasImageWidthClassMd = childClass?.includes('md:w-')
+
             return (
               <li key={path} className="py-12 last:pb-0 md:mt-12 md:py-0">
-                <article className="grid md:overflow-hidden md:rounded-md md:border-2 md:border-gray-200 md:border-opacity-60 md:dark:border-gray-700 xl:grid-cols-4">
+                <article className="grid md:grid-cols-7 md:overflow-hidden md:rounded-md md:border-2 md:border-gray-200 md:border-opacity-60 md:dark:border-gray-700 xl:grid-cols-8">
                   <dl
-                    className={`md:col-start-1 md:col-end-4 md:row-start-1 md:grid md:items-center md:p-[0.75rem] xl:col-end-2 xl:border-r-2 xl:border-gray-200 xl:border-opacity-60 xl:text-center xl:dark:border-gray-700
-                    ${!postImageSrc ? 'xl:row-end-5' : ''}`}
+                    className={`md:col-end-8 md:row-start-1 md:grid md:items-center md:p-[0.75rem] xl:border-r-2 xl:border-inherit
+                    ${url ? 'xl:row-end-2' : 'xl:row-end-5'}
+                    ${isSquare || isVertical ? 'md:col-start-3 md:border-l-2 md:border-inherit xl:col-start-3 xl:col-end-9 xl:border-none xl:pl-4' : 'md:col-start-1 xl:col-end-3 xl:text-center'}`}
                   >
                     <dt className="sr-only">Published on</dt>
                     <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                       <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                     </dd>
                   </dl>
-                  {postImageSrc && (
+                  {url && (
                     <Link
                       href={`/blog/${slug}`}
-                      className="mt-4 grid md:col-start-1 md:col-end-2 md:row-start-2 md:mt-0 md:items-center md:justify-center md:border-y-2 md:border-r-2 md:border-gray-200 md:border-opacity-60 md:p-2 md:dark:border-gray-700 xl:row-end-5 xl:border-b-0"
+                      className={`relative mt-4 grid md:col-start-1 md:col-end-3 md:mt-0 md:items-center md:justify-center md:border-inherit xl:row-end-5 xl:h-full xl:border-b-0 xl:border-r-2
+                      ${parentClass ?? ''}
+                      ${isSquare ? 'md:row-end-4 md:border-b-0 xl:row-start-1 xl:border-t-0' : 'md:row-end-3'}
+                      ${isVertical ? 'md:row-start-1 md:row-end-5 md:border-y-0 xl:aspect-[10/12]' : 'md:row-start-2 md:border-y-2'}`}
+                      style={parentStyle}
                     >
-                      <PostImage
-                        src={postImageSrc}
-                        alt=""
-                        height={162}
-                        width={216}
-                        xl={{ height: 246, width: 328 }}
+                      <Image
+                        src={url}
+                        alt={alt ?? ''}
+                        width={width}
+                        height={height}
+                        className={`pr-4 md:px-2
+                        ${childClass ?? ''}
+                        ${!hasImageWidthClassMd ? 'md:w-[1000px]' : ''}`}
+                        style={childStyle}
                       />
                     </Link>
                   )}
                   <div
-                    className={`mt-4 md:row-start-2 md:mt-0 md:border-y-2 md:border-gray-200 md:border-opacity-60 md:p-4 md:dark:border-gray-700 xl:col-start-2 xl:col-end-5 xl:row-start-1 xl:row-end-2 xl:border-none xl:pb-0
-                      ${postImageSrc ? 'md:col-start-2 md:col-end-4' : 'md:col-start-1 md:col-end-4'}`}
+                    className={`mt-4 md:row-start-2 md:mt-0 md:border-2 md:border-r-0 md:border-inherit md:p-4 xl:row-end-2 xl:border-none xl:pb-0
+                    ${url ? 'md:col-start-3 md:col-end-8 xl:col-end-9' : 'md:col-start-1 md:col-end-8 md:border-l-0 xl:col-start-3 xl:col-end-9'}
+                    ${isSquare || isVertical ? 'md:border-b-0 md:pb-0 xl:row-start-2' : 'xl:row-start-1'}`}
                   >
                     <h2 className="text-2xl font-bold leading-8 tracking-tight">
                       <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
@@ -80,10 +94,16 @@ export default function Home({ posts, pagination, initialDisplayPosts = [] }: Ho
                       ))}
                     </div>
                   </div>
-                  <div className="prose mt-6 max-w-none text-gray-500 dark:text-gray-400 md:col-start-1 md:col-end-4 md:row-start-3 md:mt-0 md:p-4 xl:col-start-2 xl:col-end-5 xl:row-start-2 xl:row-end-4">
+                  <div
+                    className={`prose mt-6 max-w-none border-inherit text-gray-500 dark:text-gray-400 md:col-end-8 md:row-start-3 md:mt-0 md:p-4 xl:col-start-3 xl:col-end-9 xl:row-start-2 xl:row-end-4
+                    ${isSquare || isVertical ? 'md:col-start-3 md:border-l-2 xl:row-start-3 xl:border-l-0' : 'md:col-start-1'}`}
+                  >
                     {summary}
                   </div>
-                  <div className="mt-6 flex justify-between text-base font-medium leading-6 md:col-start-1 md:col-end-4 md:row-start-4 md:mt-0 md:border-t-2 md:border-gray-200 md:border-opacity-60 md:dark:border-gray-700 xl:col-start-2 xl:col-end-5">
+                  <div
+                    className={`mt-6 flex justify-between text-base font-medium leading-6 md:col-end-8 md:row-start-4 md:mt-0 md:border-t-2 md:border-inherit xl:col-start-3 xl:col-end-9 xl:mt-auto
+                    ${isVertical ? 'md:col-start-3 md:border-l-2 xl:border-l-0' : 'md:col-start-1'}`}
+                  >
                     <Link
                       href={`/blog/${slug}`}
                       className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 md:w-full md:items-center md:p-[0.75rem] md:text-center md:hover:bg-primary-500 md:hover:text-primary-100 md:dark:hover:text-primary-950"
