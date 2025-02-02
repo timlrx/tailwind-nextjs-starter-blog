e80ab98ce68355a5f6da280e015dcebe1c319329
erickleite338 <erickleite338@gmail.com>
2025-02-02 01:06:29 +0000

    feat(ListLayoutWithTags): Add support for cover images
    - Added `Image` component from `next/image` to render cover images
    - Added logic to determine if the cover image is vertical and adjust the
    layout accordingly
    - Used CSS grid to organize post elements (date, image, title, tags and
    summary) in a more flexible and responsive way
    - Simplified the assignment of the `displayPosts` variable
    - Imported the `Pagination` component from an external file

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -5,66 +5,19 @@ import { slug } from 'github-slugger'
 import { formatDate } from 'pliny/utils/formatDate'
 import { CoreContent } from 'pliny/utils/contentlayer'
 import type { Blog } from 'contentlayer/generated'
+import type { PaginationProps } from '@/components/Pagination'
+import Image from 'next/image'
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
+import Pagination from '@/components/Pagination'
 import siteMetadata from '@/data/siteMetadata'
 import tagData from 'app/tag-data.json'
 
-interface PaginationProps {
-  totalPages: number
-  currentPage: number
-}
 interface ListLayoutProps {
   posts: CoreContent<Blog>[]
   title: string
   initialDisplayPosts?: CoreContent<Blog>[]
   pagination?: PaginationProps
-}
-
-function Pagination({ totalPages, currentPage }: PaginationProps) {
-  const pathname = usePathname()
-  const segments = pathname.split('/')
-  const lastSegment = segments[segments.length - 1]
-  const basePath = pathname
-    .replace(/^\//, '') // Remove leading slash
-    .replace(/\/page\/\d+$/, '') // Remove any trailing /page
-  console.log(pathname)
-  console.log(basePath)
-  const prevPage = currentPage - 1 > 0
-  const nextPage = currentPage + 1 <= totalPages
-
-  return (
-    <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-      <nav className="flex justify-between">
-        {!prevPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
-          </button>
-        )}
-        {prevPage && (
-          <Link
-            href={currentPage - 1 === 1 ? `/${basePath}/` : `/${basePath}/page/${currentPage - 1}`}
-            rel="prev"
-          >
-            Previous
-          </Link>
-        )}
-        <span>
-          {currentPage} of {totalPages}
-        </span>
-        {!nextPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
-          </button>
-        )}
-        {nextPage && (
-          <Link href={`/${basePath}/page/${currentPage + 1}`} rel="next">
-            Next
-          </Link>
-        )}
-      </nav>
-    </div>
-  )
 }
 
 export default function ListLayoutWithTags({
@@ -78,7 +31,7 @@ export default function ListLayoutWithTags({
   const tagKeys = Object.keys(tagCounts)
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
 
-  const displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
+  const displayPosts = initialDisplayPosts.length ? initialDisplayPosts : posts
 
   return (
     <>
@@ -127,32 +80,60 @@ export default function ListLayoutWithTags({
           <div>
             <ul>
               {displayPosts.map((post) => {
-                const { path, date, title, summary, tags } = post
+                const { path, date, coverImage, title, summary, tags } = post
+                const { url, alt, width, height, blog } = coverImage ?? {}
+                const { parentClass, childClass, parentStyle, childStyle } = blog ?? {}
+                const isVertical = width && height ? width < height : false
                 return (
                   <li key={path} className="py-5">
-                    <article className="flex flex-col space-y-2 xl:space-y-0">
-                      <dl>
+                    <article className="grid xl:grid-cols-8 xl:gap-x-4">
+                      <dl
+                        className={`xl:row-start-1 xl:pt-1
+                        ${isVertical ? 'xl:col-start-4 xl:col-end-9' : 'xl:col-start-1 xl:col-end-4'}`}
+                      >
                         <dt className="sr-only">Published on</dt>
-                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <dd
+                          className={`text-base font-medium leading-6 text-gray-500 dark:text-gray-400
+                          ${isVertical ? 'xl:text-left' : 'xl:text-center'}`}
+                        >
                           <time dateTime={date} suppressHydrationWarning>
                             {formatDate(date, siteMetadata.locale)}
                           </time>
                         </dd>
                       </dl>
-                      <div className="space-y-3">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags?.map((tag) => <Tag key={tag} text={tag} />)}
-                          </div>
+                      {url && (
+                        <Link
+                          href={`/${path}`}
+                          className={`mt-3 grid xl:col-start-1 xl:col-end-4 xl:mt-2 xl:justify-center
+                          ${parentClass ? parentClass : ''}
+                          ${isVertical ? 'xl:row-start-1 xl:row-end-5' : 'xl:row-start-2 xl:row-end-5'}`}
+                          style={parentStyle}
+                        >
+                          <Image
+                            src={url}
+                            alt={alt ?? ''}
+                            width={width}
+                            height={height}
+                            className={childClass}
+                            style={childStyle}
+                          />
+                        </Link>
+                      )}
+                      <div
+                        className={`mt-3 xl:col-start-4 xl:col-end-9
+                        ${isVertical ? 'xl:row-start-2 xl:row-end-3 xl:mt-3' : 'xl:row-start-1 xl:row-end-3 xl:mt-0'}`}
+                      >
+                        <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                          <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
+                            {title}
+                          </Link>
+                        </h2>
+                        <div className="flex flex-wrap">
+                          {tags?.map((tag) => <Tag key={tag} text={tag} />)}
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
-                        </div>
+                      </div>
+                      <div className="prose mt-3 max-w-none text-gray-500 dark:text-gray-400 xl:col-start-4 xl:col-end-9 xl:row-start-3 xl:row-end-5">
+                        {summary}
                       </div>
                     </article>
                   </li>
